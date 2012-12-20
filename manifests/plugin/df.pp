c013e73586f080bbc13d0c70b5e1f84dffe47c13
William Van Hevelingen <blkperl@cat.pdx.edu>
2012-12-20 13:56:50 -0800

    Add collectd df plugin support

---
+++ manifests/plugin/df.pp
@@ -0,0 +1,27 @@
+class collectd::plugin::df (
+  mountpoints,
+  fstypes,
+  ignoreselected = 'false',
+  reportbydevice = 'false',
+  reportinodes   = 'true'
+) {
+  include collectd::params
+
+  $conf_dir          = $collectd::params::plugin_conf_dir
+  $main_configs_file = $collectd::params::main_configs_file
+
+  file_line { 'include_df_conf':
+    line => "Include \"${conf_dir}/df.conf\"",
+    path => $main_configs_file
+  }
+
+  file { 'df.conf':
+    path      => "${conf_dir}/df.conf",
+    ensure    => file,
+    mode      => '0644',
+    owner     => 'root',
+    group     => 'root',
+    content   => template('collectd/df.conf.erb'),
+    subscribe => Service['collectd'],
+  }
+}
