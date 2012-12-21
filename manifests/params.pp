15f1ea74b5f91651a08a959e5f067ceeea069000
William Van Hevelingen <blkperl@cat.pdx.edu>
2012-12-20 16:07:47 -0800

    Add Solaris collectd parameters

--- manifests/params.pp
+++ manifests/params.pp
@@ -10,7 +10,12 @@ class collectd::params {
       $main_configs_file = "${collectd_dir}/collectd.conf"
     }
     'Solaris': {
-      fail('Not implemented yet.')
+      $package           = 'CSWcollectd'
+      $provider          = 'pkgutil'
+      $collectd_dir      = '/etc/opt/csw'
+      $plugin_conf_dir   = "${collectd_dir}/collectd.d"
+      $service_name      = 'collectd'
+      $main_configs_file = "/etc/opt/csw/collectd.conf"
     }
     'Redhat': {
       fail('Not implemented yet.')
