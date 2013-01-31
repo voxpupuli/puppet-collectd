171bddd043019e70180d1fff65b2751f7c53a5c8
William Van Hevelingen <blkperl@cat.pdx.edu>
2013-01-30 18:12:13 -0800

    Template openvpn plugin and parameterize the class

--- manifests/plugin/openvpn.pp
+++ manifests/plugin/openvpn.pp
@@ -1,4 +1,10 @@
-class collectd::plugin::openvpn {
+class collectd::plugin::openvpn (
+  $statusfile             = '/etc/openvpn/openvpn-status.log',
+  $improvednamingschema   = 'false',
+  $collectcompression     = 'true',
+  $collectindividualusers = 'true',
+  $collectusercount       = 'false',
+) {
   include collectd::params
 
   $conf_dir          = $collectd::params::plugin_conf_dir
