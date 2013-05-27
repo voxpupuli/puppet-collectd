628ed7889c93763b2ea7bd26d9a41a11752e713a
tripledes <tripledes@gmail.com>
2013-05-27 16:35:10 +0200

    Added configuration for StoreRates and Port.

--- manifests/plugin/write_graphite.pp
+++ manifests/plugin/write_graphite.pp
@@ -1,5 +1,7 @@
 class collectd::plugin::write_graphite (
   $graphitehost = 'localhost',
+  $storerates   = false,
+  $graphiteport = '2003',
   $ensure       = present
 ) {
   include collectd::params
