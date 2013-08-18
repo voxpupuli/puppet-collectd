79d8a68967e2b33e1639a5ac72b9cbc8bb65cd13
Andreas Jaggi <andreas.jaggi@waterwave.ch>
2013-08-19 00:59:36 +0200

    Add network plugin

---
+++ manifests/plugin/network.pp
@@ -0,0 +1,32 @@
+class collectd::plugin::network (
+  $server = undef,
+  $serverport = 25826,
+  $server_securitylevel = undef,
+  $server_username = undef,
+  $server_password = undef,
+  $server_interface = undef,
+  $listen = undef,
+  $listenport = 25826,
+  $listen_securitylevel = undef,
+  $listen_authfile = undef,
+  $listen_interface = undef,
+  $timetolive = undef,
+  $maxpacketsize = undef,
+  $forward = 'false',
+  $reportstats = 'false',
+  $ensure = present
+) {
+  include collectd::params
+
+  $conf_dir = $collectd::params::plugin_conf_dir
+
+  file { 'network.conf':
+    ensure  => $collectd::plugin::network::ensure,
+    path    => "${conf_dir}/network.conf",
+    mode    => '0644',
+    owner   => 'root',
+    group   => 'root',
+    content => template('collectd/network.conf.erb'),
+    notify  => Service['collectd']
+  }
+}
