9ceef0ef8f36bad6a33dbd58be1f972bf09a8991
William Van Hevelingen <blkperl@cat.pdx.edu>
2013-10-03 09:32:43 -0700

    Remove unneeded conditional in filecount plugin

--- manifests/plugin/filecount.pp
+++ manifests/plugin/filecount.pp
@@ -1,16 +1,13 @@
 # https://collectd.org/wiki/index.php/Plugin:FileCount
 class collectd::plugin::filecount (
   $ensure      = present,
-  $directories = undef,
+  $directories = {},
 ) {
   include collectd::params
 
   $conf_dir = $collectd::params::plugin_conf_dir
   validate_hash($directories)
 
-  if ! $directories or ! is_hash($directories) {
-    fail('directories are not properly set')
-  }
   file { 'filecount.conf':
     ensure    => $ensure,
     path      => "${conf_dir}/filecount.conf",
