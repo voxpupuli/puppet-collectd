d215327f196ee266fa0831b115678f8ce7769811
William Van Hevelingen <blkperl@cat.pdx.edu>
2013-10-02 23:17:27 -0700

    Add tail plugin

---
+++ manifests/plugin/tail.pp
@@ -0,0 +1,6 @@
+# Tail plugin
+# https://collectd.org/wiki/index.php/Plugin:Tail
+class collectd::plugin::tail {
+  collectd::plugin { 'tail': }
+}
+
---
+++ tests/plugins/tail.pp
@@ -0,0 +1,33 @@
+include collectd
+
+collectd::plugin::tail::file { 'exim-log':
+  filename => '/var/log/exim4/mainlog',
+  instance => 'exim',
+  matches  => [
+    {
+      regex    => 'S=([1-9][0-9]*)',
+      dstype   => 'CounterAdd',
+      type     => 'ipt_bytes',
+      instance => 'total',
+    },
+    {
+      regex    => '\\<R=local_user\\>',
+      dstype   => 'CounterInc',
+      type     => 'counter',
+      instance => 'local_user',
+    }
+  ]
+}
+
+collectd::plugin::tail::file { 'auth-log':
+  filename => '/var/log/auth.log',
+  instance => 'auth',
+  matches  => [
+    {
+      regex    => '\\<sshd[^:]*: Accepted publickey for [^ ]+ from\\>',
+      dstype   => 'CounterInc',
+      type     => 'counter',
+      instance => 'auth-publickey',
+    }
+  ]
+}
