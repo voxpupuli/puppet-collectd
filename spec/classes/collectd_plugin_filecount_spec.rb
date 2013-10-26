548f0891f494e85b4a10fcc57189b940354eed04
Sergio Jimenez <tripledes@gmail.com>
2013-10-27 01:31:42 +0200

    Added rspec tests for plugin filecount

---
+++ spec/classes/collectd_plugin_filecount_spec.rb
@@ -0,0 +1,42 @@
+require 'spec_helper'
+
+describe 'collectd::plugin::filecount', :type => :class do
+  let :facts do
+    {:osfamily => 'RedHat'}
+  end
+
+  context ':ensure => present and :directories => { \'active\' => \'/var/spool/postfix/active\' }' do
+    let :params do
+      {:directories => { 'active' => '/var/spool/postfix/active' }}
+    end
+    it 'Will create /etc/collectd.d/filecount.conf' do
+      should contain_file('filecount.conf').with({
+        :ensure  => 'present',
+        :path    => '/etc/collectd.d/filecount.conf',
+        :content => /Directory "\/var\/spool\/postfix\/active"/,
+      })
+    end
+  end
+
+  context ':ensure => absent' do
+    let :params do
+      {:directories => { 'active' => '/var/spool/postfix/active' }, :ensure => 'absent'}
+    end
+    it 'Will not create /etc/collectd.d/filecount.conf' do
+      should contain_file('filecount.conf').with({
+        :ensure => 'absent',
+        :path   => '/etc/collectd.d/filecount.conf',
+      })
+    end
+  end
+
+  context ':directories is not hash' do
+    let :params do
+      {:directories => '/var/spool/postfix/active'}
+    end
+    it 'Will raise an error about :directories being a String' do
+      expect {should}.to raise_error(Puppet::Error,/String/)
+    end
+  end
+end
+
