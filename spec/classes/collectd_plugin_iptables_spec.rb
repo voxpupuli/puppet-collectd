d77fdd6aabcc9583a6571c1d4198f607837f7b4c
Sergio Jimenez <tripledes@gmail.com>
2013-10-27 01:32:36 +0200

    Added rspec tests for plugin iptables

---
+++ spec/classes/collectd_plugin_iptables_spec.rb
@@ -0,0 +1,42 @@
+require 'spec_helper'
+
+describe 'collectd::plugin::iptables', :type => :class do
+  let :facts do
+    {:osfamily => 'RedHat'}
+  end
+
+  context ':ensure => present and :chains => { \'nat\' => \'In_SSH\' }' do
+    let :params do
+      {:chains => { 'nat' => 'In_SSH' }}
+    end
+    it 'Will create /etc/collectd.d/iptables.conf' do
+      should contain_file('iptables.conf').with({
+        :ensure  => 'present',
+        :path    => '/etc/collectd.d/iptables.conf',
+        :content => /Chain nat In_SSH/,
+      })
+    end
+  end
+
+  context ':ensure => absent' do
+    let :params do
+      {:chains => { 'nat' => 'In_SSH' }, :ensure => 'absent'}
+    end
+    it 'Will not create /etc/collectd.d/iptables.conf' do
+      should contain_file('iptables.conf').with({
+        :ensure => 'absent',
+        :path   => '/etc/collectd.d/iptables.conf',
+      })
+    end
+  end
+
+  context ':chains is not a hash' do
+    let :params do
+      {:chains => ['nat','In_SSH']}
+    end
+    it 'Will raise an error about :chains being an Array' do
+      expect {should}.to raise_error(Puppet::Error,/Array/)
+    end
+  end
+end
+
