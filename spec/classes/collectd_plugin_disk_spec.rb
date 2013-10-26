52efda0313701d4aa59fb8cb1555ff6b19809ef9
Sergio Jimenez <tripledes@gmail.com>
2013-10-27 01:31:17 +0200

    Added rspec tests for plugin disk

---
+++ spec/classes/collectd_plugin_disk_spec.rb
@@ -0,0 +1,42 @@
+require 'spec_helper'
+
+describe 'collectd::plugin::disk', :type => :class do
+  let :facts do
+    {:osfamily => 'RedHat'}
+  end
+
+  context ':ensure => present and :disks => [\'sda\']' do
+    let :params do
+      {:disks => ['sda']}
+    end
+    it 'Will create /etc/collectd.d/disk.conf' do
+      should contain_file('disk.conf').with({
+        :ensure  => 'present',
+        :path    => '/etc/collectd.d/disk.conf',
+        :content => /Disk  "sda"/,
+      })
+    end
+  end
+
+  context ':ensure => absent' do
+    let :params do
+      {:disks => ['sda'], :ensure => 'absent'}
+    end
+    it 'Will not create /etc/collectd.d/disk.conf' do
+      should contain_file('disk.conf').with({
+        :ensure => 'absent',
+        :path   => '/etc/collectd.d/disk.conf',
+      })
+    end
+  end
+
+  context ':disks is not an array' do
+    let :params do
+      {:disks => 'sda'}
+    end
+    it 'Will raise an error about :disks being a String' do
+      expect {should}.to raise_error(Puppet::Error,/String/)
+    end
+  end
+end
+
