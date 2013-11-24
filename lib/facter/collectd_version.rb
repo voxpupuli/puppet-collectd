3d51c429f92ed7d13f7d8ef8375e814a4fec0a85
Michael Moll <kvedulv@kvedulv.de>
2013-11-24 22:27:57 +0100

    conditionalize collectd -h run

--- lib/facter/collectd_version.rb
+++ lib/facter/collectd_version.rb
@@ -8,7 +8,8 @@
 #
 Facter.add(:collectd_version) do
   setcode do
-    if collectd_help = Facter::Util::Resolution.exec('collectd -h') and collectd_help =~ /^collectd ([0-9.]+), http:\/\/collectd.org\//
+    if Facter::Util::Resolution.which('collectd')
+      collectd_help = Facter::Util::Resolution.exec('collectd -h') and collectd_help =~ /^collectd ([0-9.]+), http:\/\/collectd.org\//
       $1
     else
       nil
