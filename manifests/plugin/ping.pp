bc3c23b1971847446e6c53f472671e334a96abcb
Kristian Glass <git@doismellburning.co.uk>
2013-11-28 12:15:10 +0000

    collectd::plugin::ping

---
+++ manifests/plugin/ping.pp
@@ -0,0 +1,26 @@
+# See http://collectd.org/documentation/manpages/collectd.conf.5.shtml#plugin_ping
+define collectd::plugin::ping (
+  $hosts,
+  $interval       = undef,
+  $timeout        = undef,
+  $ttl            = undef,
+  $source_address = undef,
+  $device         = undef,
+  $max_missed     = undef,
+) {
+  include collectd::params
+
+  validate_array($hosts)
+
+  $conf_dir = $collectd::params::plugin_conf_dir
+
+  file {
+    "${name}.load":
+      path    => "${conf_dir}/ping-${name}.conf",
+      owner   => root,
+      group   => root,
+      mode    => '0644',
+      content => template('collectd/ping.conf.erb'),
+      notify  => Service['collectd'],
+  }
+}
