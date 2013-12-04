c1cf3baa0ad0c9b20f8f7dafcdfdfe6c20939732
Tom Booth <tombooth@gmail.com>
2013-12-04 11:16:11 +0000

    Add spec test for processes plugin
    I fixed a couple of things while building this:
      - processes.load was a copy paste hangover that I didn't like
      - Process in processes.conf.erb was indented with a tab = BAD

---
+++ spec/classes/collectd_plugin_processes_spec.rb
@@ -0,0 +1,50 @@
+require 'spec_helper'
+
+describe 'collectd::plugin::processes', :type => :class do
+  let :facts do
+    {:osfamily => 'RedHat'}
+  end
+
+  context ':ensure => present, default params' do
+    it 'Will create /etc/collectd.d/processes.conf' do
+      should contain_file('processes.conf').with({
+        :ensure  => 'present',
+        :path    => '/etc/collectd.d/processes.conf',
+        :content => /<Plugin "processes">\n<\/Plugin>/,
+      })
+    end
+  end
+
+  context ':ensure => present, specific params' do
+    let :params do
+      { :processes => [ 'process1'],
+        :process_matches => [ 
+          { 'name' => 'process-all',
+            'regex' => 'process[0-9]' }
+        ],
+      }
+    end
+
+    it 'Will create /etc/collectd.d/processes.conf' do
+      should contain_file('processes.conf').with({
+        :ensure  => 'present',
+        :path    => '/etc/collectd.d/processes.conf',
+        :content => /<Plugin "processes">\n\s*Process "process1"\n\s*ProcessMatch "process-all" "process\[0-9\]"\n<\/Plugin>/,
+      })
+    end
+  end
+
+  context ':ensure => absent' do
+    let :params do
+      {:ensure => 'absent'}
+    end
+
+    it 'Will not create /etc/collectd.d/processes.conf' do
+      should contain_file('processes.conf').with({
+        :ensure => 'absent',
+        :path   => '/etc/collectd.d/processes.conf',
+      })
+    end
+  end
+end
+
