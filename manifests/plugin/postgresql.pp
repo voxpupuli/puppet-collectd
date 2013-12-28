8f456493aed2b7d43be06e2abbcc6dba2b39f5ce
Stefan Möding <stm@kill-9.net>
2013-12-28 21:00:15 +0100

    Initial version of PostgreSQL plugin

---
+++ manifests/plugin/postgresql.pp
@@ -0,0 +1,32 @@
+# https://collectd.org/wiki/index.php/Plugin:PostgreSQL
+class collectd::plugin::postgresql (
+  $ensure    = present,
+  $databases = { 'postgres' => {
+    'host'       => undef,
+    'port'       => undef,
+    'user'       => undef,
+    'password'   => undef,
+    'sslmode'    => undef,
+    'query'      => [],
+    'interval'   => undef,
+    'instance'   => undef,
+    'krbsrvname' => undef,
+    'service'    => undef,
+    }
+  },
+) {
+  include collectd::params
+
+  $conf_dir = $collectd::params::plugin_conf_dir
+  validate_hash($databases)
+
+  file { 'postgresql.conf':
+    ensure    => $collectd::plugin::postgresql::ensure,
+    path      => "${conf_dir}/postgresql.conf",
+    mode      => '0644',
+    owner     => 'root',
+    group     => $collectd::params::root_group,
+    content   => template('collectd/postgresql.conf.erb'),
+    notify    => Service['collectd']
+  }
+}
