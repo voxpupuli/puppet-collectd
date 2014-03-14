6235ab00b897d0159694d0de0ade810096c3e626
Jérôme Poittevin <jpo@atolcd.com>
2014-03-15 00:25:05 +0100

    Refactor memcached plugin using collectd::plugin resource type

--- manifests/plugin/memcached.pp
+++ manifests/plugin/memcached.pp
@@ -4,17 +4,8 @@ class collectd::plugin::memcached (
   $host   = '127.0.0.1',
   $port   = '11211',
 ) {
-  include collectd::params
-
-  $conf_dir = $collectd::params::plugin_conf_dir
-
-  file { 'memcached.conf':
-    ensure    => $ensure,
-    path      => "${conf_dir}/memcached.conf",
-    mode      => '0644',
-    owner     => 'root',
-    group     => $collectd::params::root_group,
-    content   => template('collectd/memcached.conf.erb'),
-    notify    => Service['collectd']
+  collectd::plugin {'memcached':
+    ensure  => $ensure,
+    content => template('collectd/plugin/memcached.conf.erb'),
   }
 }
