7125ad55b532aa87b412b2f5bb9b18681a48b2f5
Jérôme Poittevin <jpo@atolcd.com>
2014-03-14 12:48:13 +0100

    Refactor disk plugin using collectd::plugin resource type

--- manifests/plugin/disk.pp
+++ manifests/plugin/disk.pp
@@ -4,20 +4,12 @@ class collectd::plugin::disk (
   $disks          = [],
   $ignoreselected = false,
 ) {
-  include collectd::params
 
-  $conf_dir = $collectd::params::plugin_conf_dir
   validate_array($disks)
   validate_bool($ignoreselected)
 
-  file { 'disk.conf':
-    ensure    => $collectd::plugin::disk::ensure,
-    path      => "${conf_dir}/disk.conf",
-    mode      => '0644',
-    owner     => 'root',
-    group     => $collectd::params::root_group,
-    content   => template('collectd/disk.conf.erb'),
-    notify    => Service['collectd']
+  collectd::plugin {'disk':
+    ensure  => $ensure,
+    content => template('collectd/plugin/disk.conf.erb'),
   }
-
 }
