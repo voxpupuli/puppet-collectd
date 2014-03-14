4abc99a4f5b31f487c5516e77bc39703f29b3131
Jérôme Poittevin <jpo@atolcd.com>
2014-03-14 13:00:25 +0100

    Refactor users plugin using collectd::plugin resource type

--- manifests/plugin/users.pp
+++ manifests/plugin/users.pp
@@ -1,20 +1,9 @@
 # https://collectd.org/wiki/index.php/Plugin:Users
 class collectd::plugin::users (
-  $ensure           = present,
+  $ensure = present,
 ) {
 
-  include collectd::params
-
-  $conf_dir = $collectd::params::plugin_conf_dir
-
-  file { 'users.conf':
-    ensure    => $collectd::plugin::users::ensure,
-    path      => "${conf_dir}/users.conf",
-    mode      => '0644',
-    owner     => 'root',
-    group     => $collectd::params::root_group,
-    content   => template('collectd/users.conf.erb'),
-    notify    => Service['collectd'],
+  collectd::plugin {'users':
+    ensure => $ensure
   }
-
 }
