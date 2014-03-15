be241b0c7fa61fee9b8fc35d4a9a7fe10d7ef7c7
Jérôme Poittevin <jpo@atolcd.com>
2014-03-15 22:54:00 +0100

    Refactor uptime plugin using collectd::plugin resource type

--- manifests/plugin/uptime.pp
+++ manifests/plugin/uptime.pp
@@ -2,19 +2,7 @@
 class collectd::plugin::uptime (
   $ensure           = present,
 ) {
-
-  include collectd::params
-
-  $conf_dir = $collectd::params::plugin_conf_dir
-
-  file { 'uptime.conf':
-    ensure    => $collectd::plugin::uptime::ensure,
-    path      => "${conf_dir}/uptime.conf",
-    mode      => '0644',
-    owner     => 'root',
-    group     => $collectd::params::root_group,
-    content   => template('collectd/uptime.conf.erb'),
-    notify    => Service['collectd'],
+  collectd::plugin {'uptime':
+    ensure => $ensure
   }
-
 }
