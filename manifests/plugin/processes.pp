d347b57c3511fdf177559088cd63730abe81e80e
Jérôme Poittevin <jpo@atolcd.com>
2014-03-21 01:53:14 +0100

    Refactor processes plugin using collectd::plugin resource type

--- manifests/plugin/processes.pp
+++ manifests/plugin/processes.pp
@@ -4,21 +4,11 @@ class collectd::plugin::processes (
   $processes       = undef,
   $process_matches = undef,
 ) {
-  include collectd::params
-
   if $processes { validate_array($processes) }
   if $process_matches { validate_array($process_matches) }
 
-  $conf_dir = $collectd::params::plugin_conf_dir
-
-  file {
-    'processes.conf':
-      ensure  => $ensure,
-      path    => "${conf_dir}/processes.conf",
-      owner   => root,
-      group   => root,
-      mode    => '0644',
-      content => template('collectd/processes.conf.erb'),
-      notify  => Service['collectd'],
+  collectd::plugin {'processes':
+    ensure  => $ensure,
+    content => template('collectd/plugin/processes.conf.erb'),
   }
 }
