e457c22bb9c9ad21c0552538c17955df8139e610
Jérôme Poittevin <jpo@atolcd.com>
2014-03-22 18:12:51 +0100

    Style fixes

--- manifests/plugin/varnish.pp
+++ manifests/plugin/varnish.pp
@@ -3,15 +3,15 @@ class collectd::plugin::varnish (
   $ensure    = present,
   $instances = {
       'localhost' => {
-        'CollectCache'       => 'true',
-        'CollectBackend'     => 'true',
-        'CollectConnections' => 'true',
-        'CollectSHM'         => 'true',
-        'CollectESI'         => 'false',
-        'CollectFetch'       => 'true',
-        'CollectHCB'         => 'false',
-        'CollectTotals'      => 'true',
-        'CollectWorkers'     => 'true',
+        'CollectCache'       => true,
+        'CollectBackend'     => true,
+        'CollectConnections' => true,
+        'CollectSHM'         => true,
+        'CollectESI'         => false,
+        'CollectFetch'       => true,
+        'CollectHCB'         => false,
+        'CollectTotals'      => true,
+        'CollectWorkers'     => true,
       }
     }
 ) {
@@ -30,7 +30,7 @@ class collectd::plugin::varnish (
   }
 
   collectd::plugin {'varnish':
-    ensure => $ensure,
-    content   => template('collectd/plugin/varnish.conf.erb'),
+    ensure  => $ensure,
+    content => template('collectd/plugin/varnish.conf.erb'),
   }
 }
