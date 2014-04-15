0cb499c813a48fe6db2782a88b31f83a6715f794
Jacob Ashdown <jcbashdown@gmail.com>
2014-04-15 15:47:34 +0100

    Failing tests for non 5.4 versions
    additional test for custom config

--- spec/classes/collectd_plugin_varnish_spec.rb
+++ spec/classes/collectd_plugin_varnish_spec.rb
@@ -1,6 +1,62 @@
 require 'spec_helper'
 
 describe 'collectd::plugin::varnish', :type => :class do
+  context 'When the version is not 5.4' do
+    let :facts do
+      { :osfamily => 'RedHat',
+        :collectd_version => '5.3',
+      }
+    end
+    let :params do
+      {}
+    end
+    it 'should render the template with the default values' do
+content = <<EOS
+<Plugin varnish>
+  <Instance "localhost">
+    CollectCache true
+    CollectBackend true
+    CollectConnections true
+    CollectSHM true
+    CollectESI false
+    CollectFetch true
+    CollectHCB false
+    CollectTotals true
+    CollectWorkers true
+  </Instance>
+</Plugin>
+EOS
+      should contain_collectd__plugin('varnish').with_content(content)
+    end
+  end
+  context 'When the version is nil' do
+    let :facts do
+      { :osfamily => 'RedHat',
+        :collectd_version => nil,
+      }
+    end
+    let :params do
+      {}
+    end
+    it 'should render the template with the default values' do
+content = <<EOS
+<Plugin varnish>
+  <Instance "localhost">
+    CollectCache true
+    CollectBackend true
+    CollectConnections true
+    CollectSHM true
+    CollectESI false
+    CollectFetch true
+    CollectHCB false
+    CollectTotals true
+    CollectWorkers true
+  </Instance>
+</Plugin>
+EOS
+      should contain_collectd__plugin('varnish').with_content(content)
+    end
+  end
 
   context 'When the version is 5.4' do
     let :facts do
@@ -25,6 +81,29 @@ content = <<EOS
     CollectHCB false
     CollectTotals true
     CollectWorkers true
+  </Instance>
+</Plugin>
+EOS
+        should contain_collectd__plugin('varnish').with_content(content)
+      end
+    end
+    context 'when there are params given' do
+      let :params do
+        {
+          'instances' => {
+            'warble' => {
+              'BATMAN' => true,
+              'Robin' => false
+            }
+          }
+        }
+      end
+      it 'should render the template with the values passed in the params' do
+content = <<EOS
+<Plugin varnish>
+  <Instance "warble">
+    BATMAN true
+    Robin false
   </Instance>
 </Plugin>
 EOS
