87a9efe20255a05c07aaf0061913801c8545c14c
Jan Schumann <jan.schumann@bauermedia.com>
2014-07-09 12:43:19 +0200

    coding style

--- manifests/plugin/logfile.pp
+++ manifests/plugin/logfile.pp
@@ -2,8 +2,9 @@
 class collectd::plugin::logfile (
   $ensure        = present,
   $log_level     = 'info',
-  $log_file      = "/var/log/collectd.log",
-  $log_timestamp = true) {
+  $log_file      = '/var/log/collectd.log',
+  $log_timestamp = true
+) {
   collectd::plugin { 'logfile':
     ensure  => $ensure,
     content => template('collectd/plugin/logfile.conf.erb'),
