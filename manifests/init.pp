50436e05f45fa1aca16654a43fd2b4c17a0931ec
txaj <txaj@ovh.fr>
2014-07-11 16:01:55 -0500

    Make sure plugin_conf_dir is not world readable
    Some plugins configuration can include sensitive datas
    (passwords, tokens...). There's no legitimate reason for other users to
    sneak in those config files.

--- manifests/init.pp
+++ manifests/init.pp
@@ -26,7 +26,7 @@ class collectd(
   file { 'collectd.d':
     ensure  => directory,
     path    => $collectd::params::plugin_conf_dir,
-    mode    => '0755',
+    mode    => '0750',
     owner   => 'root',
     group   => $collectd::params::root_group,
     purge   => $purge,
