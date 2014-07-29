e5a4fd0c185b9d5677d64faf73c7c7b2ce8c8087
txaj <txaj1@ovh.fr>
2014-07-29 15:41:40 -0500

    Full rewrite of the network plugin
    It now allows more than one server and more than one listener. It can be
    configured both by parameterized class and by invoking defined types
    collectd::plugin::network::server or collectd::plugin::network::listener

---
+++ manifests/plugin/network/server.pp
@@ -0,0 +1,25 @@
+#
+define collectd::plugin::network::server (
+  $ensure        = 'present',
+  $username      = undef,
+  $password      = undef,
+  $port          = undef,
+  $securitylevel = undef,
+  $interface     = undef,
+) {
+  include collectd::params
+  include collectd::plugin::network
+
+  $conf_dir = $collectd::params::plugin_conf_dir
+
+  validate_string($name)
+
+  file { "${conf_dir}/network-server-${name}.conf":
+    ensure    => $ensure,
+    mode      => '0640',
+    owner     => 'root',
+    group     => $collectd::params::root_group,
+    content   => template('collectd/plugin/network/server.conf.erb'),
+    notify    => Service['collectd'],
+  }
+}
---
+++ spec/defines/collectd_plugin_network_listener.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe 'collectd::plugin::network::listener', :type => :define do
+  let :facts do
+    {:osfamily => 'Debian'}
+  end
+
+  context 'simple case' do
+    let(:title) { 'test' }
+    let :params do
+      {
+          :url     => 'http://www.example.com/query',
+          :matches => [{'regex' => 'SPAM \\(Score: (-?[0-9]+\\.[0-9]+)\\)', 'dstype' => 'CounterAdd', 'type' => 'counter'}],
+      }
+    end
+    it 'Will create /etc/collectd/conf.d/curl-test.conf' do
+      should contain_file('/etc/collectd/conf.d/curl-test.conf').with_content("<Plugin curl>\n  <Page \"test\">\n    URL \"http://www.example.com/query\"\n  <Match>\n    Regex \"SPAM \\(Score: (-?[0-9]+\\.[0-9]+)\\)\"\n    DSType \"CounterAdd\"\n    Type \"counter\"\n  </Match>\n\n  </Page>\n</Plugin>\n")
+    end
+  end
+
+end
