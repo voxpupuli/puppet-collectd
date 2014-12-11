3bbfe777b38f385dabf30d4c97076aceccc41cf2
txaj <txaj1@ovh.fr>
2014-12-10 23:27:48 -0700

    Add conntrack module

---
+++ manifests/plugin/conntrack.pp
@@ -0,0 +1,8 @@
+# https://collectd.org/wiki/index.php/Plugin:ConnTrack
+class collectd::plugin::conntrack (
+  $ensure = present,
+) {
+  collectd::plugin {'conntrack':
+    ensure => $ensure
+  }
+}
