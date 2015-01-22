88fb530314aa0641e6a48540b82d26d379cf9dfe
Micha Krause <micha@noris.net>
2015-01-22 11:13:15 +0100

    added CollectStatistics option to rrdcached plugin

--- manifests/plugin/rrdcached.pp
+++ manifests/plugin/rrdcached.pp
@@ -1,16 +1,17 @@
 # https://collectd.org/wiki/index.php/Rrdcached
 class collectd::plugin::rrdcached (
-  $ensure           = present,
-  $daemonaddress    = 'unix:/tmp/rrdcached.sock',
-  $datadir          = '/var/lib/rrdcached/db/collectd',
-  $createfiles      = true,
-  $createfilesasync = false,
-  $stepsize         = undef,
-  $heartbeat        = undef,
-  $interval         = undef,
-  $rrarows          = undef,
-  $rratimespan      = [],
-  $xff              = undef,
+  $ensure            = present,
+  $daemonaddress     = 'unix:/tmp/rrdcached.sock',
+  $datadir           = '/var/lib/rrdcached/db/collectd',
+  $createfiles       = true,
+  $createfilesasync  = false,
+  $stepsize          = undef,
+  $heartbeat         = undef,
+  $interval          = undef,
+  $rrarows           = undef,
+  $rratimespan       = [],
+  $xff               = undef,
+  $collectstatistics = undef,
 ) {
   validate_array($rratimespan)
   validate_bool($createfiles, $createfilesasync)
