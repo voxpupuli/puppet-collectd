8f43bd3e2daac13b9aec749071a282a4040432b3
Bernd Zeimetz <bernd@bzed.de>
2015-04-10 10:23:33 +0200

    Untested update for the python module spec test.
    Still some parts missing.

--- spec/defines/collectd_plugin_python_spec.rb
+++ spec/defines/collectd_plugin_python_spec.rb
@@ -11,25 +11,27 @@ describe 'collectd::plugin::python', :type => :define do
     let (:title) {'elasticsearch'}
     let :params do
       {
-        :modulepath    => '/usr/lib/collectd',
+        :modulepath    => '/usr/share/collectd/python',
         :module        => 'elasticsearch',
         :script_source => 'puppet:///modules/myorg/elasticsearch_collectd_python.py',
         :config        => {'Cluster' => 'elasticsearch'},
       }
     end
 
-    it 'Will create /etc/collectd/conf.d/10-elasticsearch.conf' do
-      should contain_file('elasticsearch.load').with({
+    it 'Will create /etc/collectd/conf.d/10-python.conf' do
+      should contain_file('python.load').with({
         :ensure  => 'present',
-        :path    => '/etc/collectd/conf.d/10-elasticsearch.conf',
-        :content => "<LoadPlugin \"python\">\n    Globals true\n</LoadPlugin>\n\n<Plugin \"python\">\n    ModulePath \"/usr/lib/collectd\"\n\n    Import \"elasticsearch\"\n\n    <Module \"elasticsearch\">\n        Verbose false\n\t\tCluster \"elasticsearch\"\n    </Module>\n</Plugin>\n",
+        :path    => '/etc/collectd/conf.d/10-python.conf',
+        :content => "<LoadPlugin \"python\">\n    Globals true\n</LoadPlugin>\n\n\n",
       })
     end
-    it 'Will create /usr/lib/collectd/elasticsearch.py' do
+    #it 'Will create /etc/collectd/conf.d/python-modules.conf' do
+    # FIXME - handle concat & fragments?
+    #end
+    it 'Will create /usr/share/collectd/python/elasticsearch.py' do
       should contain_file('elasticsearch.script').with({
         :ensure  => 'present',
-        :path    => '/usr/lib/collectd/elasticsearch.py',
-        #:content => "<Plugin network>\n  <Server \"node1\" \"1234\">\n    SecurityLevel \"Encrypt\"\n    Username \"foo\"\n    Password \"bar\"\n    Interface \"eth0\"\n\n  </Server>\n</Plugin>\n",
+        :path    => '/usr/share/collectd/python/elasticsearch.py',
       })
     end
   end
@@ -44,22 +46,22 @@ describe 'collectd::plugin::python', :type => :define do
     let :params do
       {
         :ensure        => 'absent',
-        :modulepath    => '/usr/lib/collectd',
+        :modulepath    => '/usr/share/collectd/python',
         :module        => 'elasticsearch',
         :script_source => 'puppet:///modules/myorg/elasticsearch_collectd_python.py',
         :config        => {'Cluster' => 'elasticsearch'},
       }
     end
-    it 'Will not create /etc/collectd/conf.d/10-elasticsearch.conf' do
-      should contain_file('elasticsearch.load').with({
+    it 'Will not create /etc/collectd/conf.d/10-python.conf' do
+      should contain_file('python.load').with({
         :ensure => 'absent',
         :path    => '/etc/collectd/conf.d/10-elasticsearch.conf',
       })
     end
-    it 'Will not create /usr/lib/collectd/elasticsearch.py' do
+    it 'Will not create /usr/share/collectd/python/elasticsearch.py' do
       should contain_file('elasticsearch.script').with({
         :ensure => 'absent',
-        :path    => '/usr/lib/collectd/elasticsearch.py',
+        :path    => '/usr/share/collectd/python/elasticsearch.py',
       })
     end
   end
