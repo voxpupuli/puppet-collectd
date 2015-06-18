6c101d5c4bcb9b354dba3a9ccd3f12ff1db67c37
Bernd Zeimetz <bernd@bzed.de>
2015-06-18 18:46:56 +0200

    Use concat to define the process plugin config.
    Adding a parser function to keep it compatible with the old way
    to define process matches.

--- manifests/plugin/processes.pp
+++ manifests/plugin/processes.pp
@@ -10,7 +10,41 @@ class collectd::plugin::processes (
 
   collectd::plugin {'processes':
     ensure   => $ensure,
-    content  => template('collectd/plugin/processes.conf.erb'),
-    interval => $interval,
   }
+
+  concat{"${collectd::params::plugin_conf_dir}/processes-config.conf":
+    ensure         => $ensure,
+    mode           => '0640',
+    owner          => 'root',
+    group          => $collectd::params::root_group,
+    notify         => Service['collectd'],
+    ensure_newline => true,
+  }
+  concat::fragment{'collectd_plugin_processes_conf_header':
+    ensure  => $ensure,
+    order   => '00',
+    content => '<Plugin processes>',
+    target  => "${collectd::params::plugin_conf_dir}/processes-config.conf",
+  }
+  concat::fragment{'collectd_plugin_processes_conf_footer':
+    ensure  => $ensure,
+    order   => '99',
+    content => '</Plugin>',
+    target  => "${collectd::params::plugin_conf_dir}/processes-config.conf",
+  }
+
+
+  if $processes {
+    collectd::plugin::processes::process { $processes : }
+  }
+  if $process_matches {
+    $process_matches_resources = collectd_convert_processmatch($process_matches)
+    $defaults = { 'ensure' => $ensure }
+    create_resources(
+      collectd::plugin::processes::processmatch,
+      $process_matches_resources,
+      $defaults
+    )
+  }
+
 }
