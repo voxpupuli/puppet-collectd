f7af2f2a64aa87b1cebc3f1d551dce79c0c605bb
Chris Heerschap <cheerschap@fiberlink.com>
2015-07-24 15:37:54 -0400

    adding ValuesAbsolute and ValuesPercentage to memory

--- manifests/plugin/memory.pp
+++ manifests/plugin/memory.pp
@@ -1,10 +1,19 @@
 # https://collectd.org/wiki/index.php/Plugin:Memory
 class collectd::plugin::memory (
-  $ensure   = present,
-  $interval = undef,
+  $ensure           = present,
+  $valuesabsolute   = true,
+  $valuespercentage = false,
+  $interval         = undef,
 ) {
+
+  validate_bool(
+    $valuesabsolute,
+    $valuespercentage,
+  )
+
   collectd::plugin {'memory':
     ensure   => $ensure,
+    content  => template('collectd/plugin/memory.conf.erb'),
     interval => $interval,
   }
 }
