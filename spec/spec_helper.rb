2ee7ecc900de78ed6226cd35d44338ced87f3524
William Van Hevelingen <william.vanhevelingen@acquia.com>
2015-08-05 20:34:52 -0700

    Fix rspec deprecation warnings

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,6 @@
 require 'puppetlabs_spec_helper/module_spec_helper'
 
 RSpec.configure do |c|
-  c.treat_symbols_as_metadata_keys_with_true_values = true
   c.include PuppetlabsSpec::Files
 
   c.before :each do
