02d919e30c0fb4ea4b534b660d292da9eb33450c
Richard Hillmann <rhillmann@intelliad.de>
2015-08-12 22:34:49 +0200

    initial dbi plugin
    dbi plugin does not support own interval
    
    fix include of plugin dbi
    
    add metadatafrom for query results
    
    update readme for dbi
    
    fix parameter interval for load plugin
    
    add rspec for plugin dbi
    
    fix creation of arrays in templates
    
    fix rspec test
    
    fix typo in and extend spec
    
    fix spec for valuesfrom

---
+++ manifests/plugin/dbi/query.pp
@@ -0,0 +1,21 @@
+#
+define collectd::plugin::dbi::query (
+  $statement,
+  $ensure     = 'present',
+  $results    = [{}],
+  $minversion = undef,
+  $maxversion = undef,
+){
+  include collectd::params
+  include collectd::plugin::dbi
+
+  validate_string($statement)
+  validate_array($results)
+
+  concat::fragment{"collectd_plugin_dbi_conf_query_${title}":
+    ensure  => $ensure,
+    order   => '30',
+    target  => "${collectd::params::plugin_conf_dir}/dbi-config.conf",
+    content => template('collectd/plugin/dbi/query.conf.erb'),
+  }
+}
