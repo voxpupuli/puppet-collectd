8a76f28e7befbefc5a2b9015b1d2f362683aae89
Christoph Maser <christoph.maser@br.de>
2015-08-31 17:10:04 +0200

    add docs and test

---
+++ spec/classes/collectd_plugin_ceph_spec.rb
@@ -0,0 +1,56 @@
+require 'spec_helper'
+
+
+describe 'collectd::plugin::ceph', :type => :class do
+  let :facts do
+    {:osfamily => 'RedHat'}
+  end
+
+  context ':ensure => present and :osds => [ \'osd.0\, \osd.1\, \osd.2\]' do
+    let :params do
+      {:osds => [ 'osd.0', 'osd.1', 'osd.2']}
+    end
+content = <<EOS
+<Plugin ceph>
+  LongRunAvgLatency 
+  ConvertSpecialMetricTypes 
+
+  <Daemon "osd.0">
+    SocketPath "/var/run/ceph/ceph-osd.0.asok"
+  </Daemon>
+  <Daemon "osd.1">
+    SocketPath "/var/run/ceph/ceph-osd.1.asok"
+  </Daemon>
+  <Daemon "osd.2">
+    SocketPath "/var/run/ceph/ceph-osd.2.asok"
+  </Daemon>
+
+</Plugin>
+EOS
+    it 'Will create /etc/collectd.d/10-ceph.conf' do
+      should contain_collectd__plugin('ceph').with_content(content)
+    end
+  end
+
+  context ':ensure => absent' do
+    let :params do
+      {:osds => [ 'osd.0', 'osd.1', 'osd.2' ], :ensure => 'absent'}
+    end
+    it 'Will not create /etc/collectd.d/10-ceph.conf' do
+      should contain_file('ceph.load').with({
+        :ensure => 'absent',
+        :path   => '/etc/collectd.d/10-ceph.conf',
+      })
+    end
+  end
+
+  context ':ceph is not an array' do
+    let :params do
+      {:osds => 'osd.0'}
+    end
+    it 'Will raise an error about :osds being a String' do
+      should compile.and_raise_error(/String/)
+    end
+  end
+
+end
---
+++ tests/plugins/ceph.pp
@@ -0,0 +1,7 @@
+include collectd 
+
+class { 'collectd::plugin::ceph':
+  osds          => [ 'osd.0', 'osd.1', 'osd.2'],
+}
+
+
