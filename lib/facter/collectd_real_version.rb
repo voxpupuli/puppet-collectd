d59e2c4772a482516655db7c1be9c45c66e59263
Harm Doedens <doedens.h@tkppensioen.nl>
2015-11-03 13:37:18 +0100

    using && instead of and

--- lib/facter/collectd_real_version.rb
+++ lib/facter/collectd_real_version.rb
@@ -9,7 +9,7 @@
 Facter.add(:collectd_real_version) do
   setcode do
     if Facter::Util::Resolution.which('collectd')
-      collectd_help = Facter::Util::Resolution.exec('collectd -h') and collectd_help =~ %r{^collectd ([\w.]+), http://collectd\.org/}
+      collectd_help = Facter::Util::Resolution.exec('collectd -h') && collectd_help =~ %r{^collectd ([\w.]+), http://collectd\.org/}
       Regexp.last_match(1)
     end
   end
