cd3a2cd8a4916c15daf94c0c5b026bda86481dfe
Éric Lemoine <elemoine@mirantis.com>
2016-01-15 07:55:25 +0100

    Make module.conf.erb support complex config
    This makes the module.conf.erb template support complex config, i.e. config
    with values that are arrays or hashes.
    
    Closes #390

--- spec/defines/collectd_plugin_python_module_spec.rb
+++ spec/defines/collectd_plugin_python_module_spec.rb
@@ -81,4 +81,21 @@ describe 'collectd::plugin::python::module', :type => :define do
                                              :path    => '/usr/share/collectd/python/foo.py',)
     end
   end
+
+  context 'complex config' do
+    let(:title) { 'foo' }
+    let :params do
+      {
+        :config => {'k1' => 'v1', 'k2' => ['v21', 'v22'], 'k3' => {'k31' => 'v31', 'k32' => 'v32'}},
+      }
+    end
+
+    it 'includes foo module configuration' do
+      should contain_concat__fragment('collectd_plugin_python_conf_foo').with(:content => /k1 v1/,)
+      should contain_concat__fragment('collectd_plugin_python_conf_foo').with(:content => /k2 v21/,)
+      should contain_concat__fragment('collectd_plugin_python_conf_foo').with(:content => /k2 v22/,)
+      should contain_concat__fragment('collectd_plugin_python_conf_foo').with(:content => /k3 k31 v31/,)
+      should contain_concat__fragment('collectd_plugin_python_conf_foo').with(:content => /k3 k32 v32/,)
+    end
+  end
 end
