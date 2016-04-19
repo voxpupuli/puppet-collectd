b0ad2994ed702f614805f7d56fcea7b1f6a36a8a
Joseph Yaworski <joseph.yaworski@fireeye.com>
2016-04-18 21:46:27 -0400

    Ensure collectd-disk exists on RedHat and collectd >= 5.5

--- manifests/plugin/disk.pp
+++ manifests/plugin/disk.pp
@@ -1,9 +1,11 @@
 # https://collectd.org/wiki/index.php/Plugin:Disk
 class collectd::plugin::disk (
-  $ensure         = 'present',
   $disks          = [],
+  $ensure         = 'present',
   $ignoreselected = false,
   $interval       = undef,
+  $manage_package = undef,
+  $package_name   = 'collectd-disk',
   $udevnameattr   = undef,
 ) {
 
@@ -11,6 +13,25 @@ class collectd::plugin::disk (
 
   validate_array($disks)
   validate_bool($ignoreselected)
+
+  if $::osfamily == 'RedHat' {
+    if $manage_package {
+      $_manage_package = $manage_package
+    } else {
+      if versioncmp($::collectd::collectd_version_real, '5.5') >= 0 {
+        $_manage_package = true
+    } else {
+        $_manage_package = false
+      }
+    }
+
+    if $_manage_package {
+      package { 'collectd-disk':
+        ensure => $ensure,
+        name   => $package_name,
+      }
+    }
+  }
 
   collectd::plugin { 'disk':
     ensure   => $ensure,
