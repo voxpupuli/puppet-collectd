75c591de64e922b48286114cb17f61d4f98e02e9
archie.harris <archie.harris@gmail.com>
2016-04-27 17:18:45 +1000

    initial spec tests for write_riemann plugin class

---
+++ spec/classes/collectd_plugin_write_riemann_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+describe 'collectd::plugin::write_riemann', type: :class do
+  let :facts do
+    {
+      osfamily: 'RedHat',
+      collectd_version: '5.5.0',
+    }
+  end
+
+  context ':ensure => present and :riemann_host => \'myhost\'' do
+    let :params do
+      { riemann_host: 'myhost', riemann_port: '5555', protocol: 'TCP', batch: 'false' }
+    end
+    it 'Will create /etc/collectd.d/10-write_riemann.conf' do
+      should contain_file('write_riemann.load').with(ensure: 'present',)
+      should contain_file('write_riemann.load').with(path: '/etc/collectd.d/10-write_riemann.conf',)
+      should contain_file('write_riemann.load').with(content: /Host "myhost"/,)
+      should contain_file('write_riemann.load').with(content: /Port "5555"/,)
+      should contain_file('write_riemann.load').with(content: /Protocol TCP/,)
+      should contain_file('write_riemann.load').with(content: /Batch false/,)
+    end
+  end
+
+  context ':ensure => absent' do
+    let :params do
+      { riemann_host: ['myhost'], ensure: 'absent' }
+    end
+    it 'Will not create ' do
+      should contain_file('write_riemann.load').with(ensure: 'absent',
+                                                     path: '/etc/collectd.d/10-write_riemann.conf',)
+    end
+  end
+end
