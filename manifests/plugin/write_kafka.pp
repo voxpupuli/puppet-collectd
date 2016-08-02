3b0afd300cd9b72a8bc77d6e4c894fa3ebc82cea
Fabian Zimmermann <dev.faz@googlemail.com>
2016-08-02 10:49:00 +0200

    plugin/write_kafka.pp:
    added kafka_hosts param which allows usage of hosts-array
    kafka_host is still valid and will be merged correctly
    default (is nothing else given) will still be localhost

--- manifests/plugin/write_kafka.pp
+++ manifests/plugin/write_kafka.pp
@@ -1,6 +1,7 @@
 class collectd::plugin::write_kafka (
   $ensure     = 'present',
-  $kafka_host = 'localhost',
+  $kafka_host = undef,
+  $kafka_hosts = ['localhost'],
   $kafka_port = 9092,
   $topics     = {},
   $interval   = undef,
@@ -9,8 +10,15 @@ class collectd::plugin::write_kafka (
   include ::collectd
 
   validate_hash($topics)
+  validate_array($kafka_hosts)
 
-  $kafka_broker = "${kafka_host}:${kafka_port}"
+  if ($kafka_host or $kafka_hosts) {
+    if($kafka_host) {
+      $real_kafka_host = [ "${kafka_host}:${kafka_port}" ]
+    }
+    $real_kafka_hosts = concat($kafka_hosts, $real_kafka_host)
+  }
+  $kafka_broker = join($real_kafka_hosts, ',')
 
   collectd::plugin { 'write_kafka':
     ensure   => $ensure,
