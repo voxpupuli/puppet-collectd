e2a0ad3020f0fbf565e6234da81768c8e84d0fde
Alexander Fisher <alex@linfratech.co.uk>
2016-10-31 12:07:35 +0000

    rubocop: fix Lint/DuplicateCaseCondition

--- spec/spec_helper_methods.rb
+++ spec/spec_helper_methods.rb
@@ -6,7 +6,7 @@ def os_specific_options(facts)
     { package: 'CSWcollectd', service: 'cswcollectd', plugin_conf_dir: '/etc/opt/csw/collectd.d' }
   when 'FreeBSD'
     { package: 'collectd5', service: 'collectd', plugin_conf_dir: '/usr/local/etc/collectd' }
-  when 'Archlinux', 'RedHat', 'Gentoo'
+  when 'Archlinux', 'RedHat'
     { package: 'collectd', service: 'collectd', plugin_conf_dir: '/etc/collectd.d' }
   when 'Debian'
     { package: 'collectd', service: 'collectd', plugin_conf_dir: '/etc/collectd/conf.d' }
