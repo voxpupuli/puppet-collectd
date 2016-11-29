9f9679afea3d2c68306aae3f0443d8ca3dc918e1
Jon Skarpeteig <jskarpet@cisco.com>
2016-11-29 10:27:24 +0100

    Python3 compatible syntax

--- lib/facter/python_dir.rb
+++ lib/facter/python_dir.rb
@@ -1,5 +1,5 @@
 Facter.add(:python_dir) do
   setcode do
-    Facter::Util::Resolution.exec('python -c "import site; print site.getsitepackages()[0]"')
+    Facter::Util::Resolution.exec('python -c "import site; print(site.getsitepackages()[0])"')
   end
 end
