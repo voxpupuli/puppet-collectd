642d7b52e2a49c65fe30d3872f9b414e74654bb0
markasammut <mark.sammut@gamesysnetwork.com>
2016-11-30 17:48:49 +0100

    ensure same version as collectd

--- manifests/plugin/curl.pp
+++ manifests/plugin/curl.pp
@@ -10,10 +10,16 @@ class collectd::plugin::curl (
 
   $_manage_package = pick($manage_package, $::collectd::manage_package)
 
+  if $ensure == 'present' {
+    $ensure_real = $::collectd::package_ensure
+  } elsif $ensure == 'absent' {
+    $ensure_real = 'absent'
+  }
+  
   if $::osfamily == 'Redhat' {
     if $_manage_package {
       package { 'collectd-curl':
-        ensure => $ensure,
+        ensure => $ensure_real,
       }
     }
   }
