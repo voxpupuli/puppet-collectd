27627a9ea1cadbee24542ca0537c63939ceec42d
Jon Skarpeteig <jskarpet@cisco.com>
2016-12-02 12:51:58 +0100

    Fallback to empty string if Python is not installed
    Avoids issue with variable undefined
    E.G: Error: Evaluation Error: Unknown variable: '::python_dir'.

--- lib/facter/python_dir.rb
+++ lib/facter/python_dir.rb
@@ -1,5 +1,13 @@
+# Fact: python_dir
+#
+# Purpose: Retrieve python package dir used by pip install
+#
 Facter.add(:python_dir) do
   setcode do
-    Facter::Util::Resolution.exec('python -c "import site; print(site.getsitepackages()[0])"')
+    if Facter::Util::Resolution.which('python')
+      Facter::Util::Resolution.exec('python -c "import site; print(site.getsitepackages()[0])"')
+    else
+      ''
+    end
   end
 end
