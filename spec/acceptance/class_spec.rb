8b30506740a14fee4cb136d14da7e94efa27b7f3
Jon Skarpeteig <jskarpet@cisco.com>
2016-12-13 11:50:54 +0100

    Don't override system default mode

--- spec/acceptance/class_spec.rb
+++ spec/acceptance/class_spec.rb
@@ -36,5 +36,9 @@ describe 'collectd class' do
       apply_manifest(pp, catch_failures: true)
       apply_manifest(pp, catch_changes: true)
     end
+
+    describe service('collectd') do
+      it { is_expected.to be_running }
+    end
   end
 end
