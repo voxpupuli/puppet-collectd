2deec729a7f902126e382d12f3786d679f610553
Thomas Lapinski <thomas.lapinski@tomtom.com>
2016-12-20 14:08:02 +0100

    Fix python_dir fact for RedHat.
    Use the correct method (value) for retrieving osfamily fact.
    Add test for RedHat to confirm conditional is working correctly.

--- spec/unit/python_dir_spec.rb
+++ spec/unit/python_dir_spec.rb
@@ -13,6 +13,17 @@ describe 'python_dir', type: :fact do
         expect(Facter.value(:python_dir)).to eq('/usr/local/lib/python2.7/dist-packages')
       end
     end
+
+    context 'RedHat' do
+      before do
+        Facter.fact(:osfamily).stubs(:value).returns('RedHat')
+        Facter::Util::Resolution.stubs(:which).with('python').returns(true)
+        Facter::Util::Resolution.stubs(:exec).with('python -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())"').returns('/usr/lib/python2.7/site-packages')
+      end
+      it do
+        expect(Facter.value(:python_dir)).to eq('/usr/lib/python2.7/site-packages')
+      end
+    end
   end
 
   it 'is empty string if python not installed' do
