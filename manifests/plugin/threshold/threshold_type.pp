4c5168cb1a5a09f42b77252c31b24f690871fec3
James Turnbull <james@lovedthanlost.net>
2017-01-11 14:05:51 -0500

    Updated type

--- manifests/plugin/threshold/threshold_type.pp
+++ manifests/plugin/threshold/threshold_type.pp
@@ -1,30 +1,12 @@
 define collectd::plugin::threshold::threshold_type (
-  $threshold_type = $name,
-  $threshold_instance = undef,
-  $warningmin     = undef,
-  $warningmax     = undef,
-  $failuremin     = undef,
-  $failuremax     = undef,
-  $hits           = undef,
-  $hysteresis     = undef,
-  $invert         = false,
-  $persist        = false,
-  $persistok      = false,
-  $percentage     = false,
-  $interesting    = true,
-  $type_instance  = undef,
-  $datasource     = undef,
+  $values             = [],
   $ensure         = 'present'
 ) {
 
   include ::collectd::plugin::threshold
   include ::collectd
 
-  validate_bool($invert)
-  validate_bool($persist)
-  validate_bool($persistok)
-  validate_bool($percentage)
-  validate_bool($interesting)
+  validate_array($values)
 
   $conf_file = "${collectd::plugin_conf_dir}/threshold-type-${title}.conf"
 
@@ -37,19 +19,19 @@ define collectd::plugin::threshold::threshold_type (
     ensure_newline => true,
   }
 
-  concat::fragment { "collectd_plugin_threshold_conf_header_${threshold_type}":
+  concat::fragment { "collectd_plugin_threshold_conf_header_${title}":
     order   => '00',
     content => '<Plugin "threshold">',
     target  => "${conf_file}",
   }
 
-  concat::fragment { "collectd_plugin_threshold_conf_threshold_type_${threshold_type}":
+  concat::fragment { "collectd_plugin_threshold_conf_threshold_type_${title}":
     order   => 50,
     content => template('collectd/plugin/threshold/threshold_type.conf.erb'),
     target  => "${conf_file}",
   }
 
-  concat::fragment { "collectd_plugin_threshold_conf_footer_${threshold_type}":
+  concat::fragment { "collectd_plugin_threshold_conf_footer_${title}":
     order   => '99',
     content => '</Plugin>',
     target  => "${conf_file}",
