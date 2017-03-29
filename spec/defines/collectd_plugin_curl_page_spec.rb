4bd3812ed681a2ff3ec98ab74d10588ad0969a0b
azhurbilo <artemzhurbilo@gmail.com>
2017-03-29 14:35:07 +0300

    add $measureresponsecode for curl plugin

--- spec/defines/collectd_plugin_curl_page_spec.rb
+++ spec/defines/collectd_plugin_curl_page_spec.rb
@@ -19,11 +19,12 @@ describe 'collectd::plugin::curl::page', type: :define do
     let :params do
       {
         url: 'http://www.example.com/query',
-        matches: [{ 'regex' => 'SPAM \\(Score: (-?[0-9]+\\.[0-9]+)\\)', 'dstype' => 'CounterAdd', 'type' => 'counter' }]
+        matches: [{ 'regex' => 'SPAM \\(Score: (-?[0-9]+\\.[0-9]+)\\)', 'dstype' => 'CounterAdd', 'type' => 'counter' }],
+        measureresponsecode: true
       }
     end
     it 'Will create /etc/collectd/conf.d/curl-test.conf' do
-      is_expected.to contain_file('/etc/collectd/conf.d/curl-test.conf').with_content("<Plugin curl>\n  <Page \"test\">\n    URL \"http://www.example.com/query\"\n  <Match>\n    Regex \"SPAM \\(Score: (-?[0-9]+\\.[0-9]+)\\)\"\n    DSType \"CounterAdd\"\n    Type \"counter\"\n  </Match>\n\n  </Page>\n</Plugin>\n")
+      is_expected.to contain_file('/etc/collectd/conf.d/curl-test.conf').with_content("<Plugin curl>\n  <Page \"test\">\n    URL \"http://www.example.com/query\"\n    MeasureResponseCode true\n  <Match>\n    Regex \"SPAM \\(Score: (-?[0-9]+\\.[0-9]+)\\)\"\n    DSType \"CounterAdd\"\n    Type \"counter\"\n  </Match>\n\n  </Page>\n</Plugin>\n")
     end
   end
 end
