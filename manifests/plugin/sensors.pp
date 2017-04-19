174a3ce84ca409382713213d801ee41cec74d2b9
Jo Vanvoorden <jo.vanvoorden@kuleuven.be>
2017-04-19 14:18:59 +0200

    Add install_options for RedHat Family in sensors plugin

--- manifests/plugin/sensors.pp
+++ manifests/plugin/sensors.pp
@@ -1,21 +1,27 @@
 # https://collectd.org/wiki/index.php/Plugin:Sensors
 class collectd::plugin::sensors (
-  $ensure           = 'present',
-  $manage_package   = undef,
-  $sensorconfigfile = undef,
-  $sensor           = undef,
-  $ignoreselected   = undef,
-  $interval         = undef,
+  $ensure                  = 'present',
+  $manage_package          = undef,
+  $sensorconfigfile        = undef,
+  $sensor                  = undef,
+  $ignoreselected          = undef,
+  $interval                = undef,
+  $package_install_options = $collectd::package_install_options,
 ) {
 
   include ::collectd
 
   $_manage_package = pick($manage_package, $::collectd::manage_package)
 
+  if $package_install_options != undef {
+    validate_array($package_install_options)
+  }
+
   if $::osfamily == 'Redhat' {
     if $_manage_package {
       package { 'collectd-sensors':
-        ensure => $ensure,
+        ensure          => $ensure,
+        install_options => $package_install_options,
       }
     }
   }
