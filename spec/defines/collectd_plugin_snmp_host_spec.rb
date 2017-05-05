d3145e1b52266e00a8309c112a0b6079c5e6e44c
Matt Dainty <matt@bodgit-n-scarper.com>
2017-05-05 15:16:00 +0100

    Update tests with some SNMPv3 examples

--- spec/defines/collectd_plugin_snmp_host_spec.rb
+++ spec/defines/collectd_plugin_snmp_host_spec.rb
@@ -36,7 +36,7 @@ describe 'collectd::plugin::snmp::host', type: :define do
         it { is_expected.to contain_file(filename).without_content(%r{Interval \d+}) }
       end
 
-      context 'all params set' do
+      context 'all SNMPv2 params set' do
         let(:params) do
           required_params.merge(address: 'bar.example.com',
                                 version: '2',
@@ -47,6 +47,30 @@ describe 'collectd::plugin::snmp::host', type: :define do
         it { is_expected.to contain_file(filename).with_content(%r{Address "bar\.example\.com"}) }
         it { is_expected.to contain_file(filename).with_content(%r{Version 2}) }
         it { is_expected.to contain_file(filename).with_content(%r{Community "opensesame"}) }
+        it { is_expected.to contain_file(filename).with_content(%r{Interval 30}) }
+      end
+
+      context 'all SNMPv3 params set' do
+        let(:params) do
+          required_params.merge(address: 'bar.example.com',
+                                version: 3,
+                                username: 'collectd',
+                                security_level: 'authPriv',
+                                auth_protocol: 'SHA',
+                                auth_passphrase: 'mekmitasdigoat',
+                                privacy_protocol: 'AES',
+                                privacy_passphrase: 'mekmitasdigoat',
+                                interval: 30)
+        end
+
+        it { is_expected.to contain_file(filename).with_content(%r{Address "bar\.example\.com"}) }
+        it { is_expected.to contain_file(filename).with_content(%r{Version 3}) }
+        it { is_expected.to contain_file(filename).with_content(%r{Username "collectd"}) }
+        it { is_expected.to contain_file(filename).with_content(%r{SecurityLevel "authPriv"}) }
+        it { is_expected.to contain_file(filename).with_content(%r{AuthProtocol "SHA"}) }
+        it { is_expected.to contain_file(filename).with_content(%r{AuthPassphrase "mekmitasdigoat"}) }
+        it { is_expected.to contain_file(filename).with_content(%r{PrivacyProtocol "AES"}) }
+        it { is_expected.to contain_file(filename).with_content(%r{PrivacyPassphrase "mekmitasdigoat"}) }
         it { is_expected.to contain_file(filename).with_content(%r{Interval 30}) }
       end
 
