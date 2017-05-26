90347775e5fb09f28db29162d95a13ae7c75f5d3
Tim Meusel <tim@bastelfreak.de>
2017-05-26 16:43:09 +0200

    migrate collectd_plugin_load tests to rspec-puppet-facts

--- spec/classes/collectd_plugin_load_spec.rb
+++ spec/classes/collectd_plugin_load_spec.rb
@@ -1,29 +1,29 @@
 require 'spec_helper'
 
 describe 'collectd::plugin::load', type: :class do
-  let :pre_condition do
-    'include ::collectd'
-  end
+  on_supported_os.each do |os, facts|
+    context "on #{os} " do
+      let :facts do
+        facts
+      end
 
-  let :facts do
-    {
-      osfamily: 'Debian',
-      collectd_version: '4.10.1',
-      operatingsystemmajrelease: '7',
-      python_dir: '/usr/local/lib/python2.7/dist-packages'
-    }
-  end
+      options = os_specific_options(facts)
+      let :pre_condition do
+        'include ::collectd'
+      end
 
-  context 'report_relative in load.conf' do
-    let :params do
-      {
-        report_relative: true
-      }
-    end
+      context 'report_relative in load.conf' do
+        let :params do
+          {
+            report_relative: true
+          }
+        end
 
-    it 'is present' do
-      is_expected.to contain_file('load.load').
-        without_content(%r{\s{2}ReportRelative true\s{2}})
+        it 'is present' do
+          is_expected.to contain_file('load.load').
+            without_content(%r{\s{2}ReportRelative true\s{2}})
+        end
+      end
     end
   end
 end
