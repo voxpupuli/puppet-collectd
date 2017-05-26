a5e5ef4174417ab25db31865bdba1c0b94c5dbc8
Tim Meusel <tim@bastelfreak.de>
2017-05-26 14:40:25 +0200

    migrate collectd_plugin_genericjmx tests to rspec-puppet-facts

--- spec/classes/collectd_plugin_genericjmx_spec.rb
+++ spec/classes/collectd_plugin_genericjmx_spec.rb
@@ -1,77 +1,74 @@
 require 'spec_helper'
 
 describe 'collectd::plugin::genericjmx', type: :class do
-  let(:facts) do
-    {
-      osfamily: 'Debian',
-      id: 'root',
-      concat_basedir: '/dne',
-      path: '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-      collectd_version: '4.8.0',
-      operatingsystemmajrelease: '7',
-      python_dir: '/usr/local/lib/python2.7/dist-packages'
-    }
-  end
+  on_supported_os.each do |os, facts|
+    context "on #{os} " do
+      let :facts do
+        facts
+      end
 
-  let(:config_filename) { '/etc/collectd/conf.d/15-genericjmx.conf' }
+      options = os_specific_options(facts)
 
-  context 'defaults' do
-    it 'will include the java plugin' do
-      is_expected.to contain_class('collectd::plugin::java')
-    end
+      let(:config_filename) { "#{options[:plugin_conf_dir]}/15-genericjmx.conf" }
 
-    it 'will load the genericjmx plugin' do
-      is_expected.to contain_concat(config_filename).
-        with(ensure: 'present',
-             mode: '0640',
-             owner: 'root',
-             group: 'root',
-             ensure_newline: true)
-    end
+      context 'defaults' do
+        it 'will include the java plugin' do
+          is_expected.to contain_class('collectd::plugin::java')
+        end
 
-    it { is_expected.to contain_concat(config_filename).that_notifies('Service[collectd]') }
-    it { is_expected.to contain_concat(config_filename).that_requires('File[collectd.d]') }
+        it 'will load the genericjmx plugin' do
+          is_expected.to contain_concat(config_filename).
+            with(ensure: 'present',
+                 mode: '0640',
+                 owner: 'root',
+                 ensure_newline: true)
+        end
 
-    it do
-      is_expected.to contain_concat__fragment('collectd_plugin_genericjmx_conf_header').
-        with(order: '00',
-             target: config_filename,
-             content: %r{<Plugin "java">.+LoadPlugin "org\.collectd\.java\.GenericJMX".+<Plugin "GenericJMX">}m)
-    end
+        it { is_expected.to contain_concat(config_filename).that_notifies('Service[collectd]') }
+        it { is_expected.to contain_concat(config_filename).that_requires('File[collectd.d]') }
 
-    it do
-      is_expected.to contain_concat__fragment('collectd_plugin_genericjmx_conf_footer').
-        with(order: '99',
-             target: config_filename,
-             content: %r{</Plugin>.+</Plugin>}m)
-    end
-  end
+        it do
+          is_expected.to contain_concat__fragment('collectd_plugin_genericjmx_conf_header').
+            with(order: '00',
+                 target: config_filename,
+                 content: %r{<Plugin "java">.+LoadPlugin "org\.collectd\.java\.GenericJMX".+<Plugin "GenericJMX">}m)
+        end
 
-  context 'jvmarg parameter array' do
-    let(:params) { { jvmarg: %w[foo bar baz] } }
+        it do
+          is_expected.to contain_concat__fragment('collectd_plugin_genericjmx_conf_footer').
+            with(order: '99',
+                 target: config_filename,
+                 content: %r{</Plugin>.+</Plugin>}m)
+        end
+      end
 
-    it 'has multiple jvmarg parameters' do
-      is_expected.to contain_concat__fragment('collectd_plugin_genericjmx_conf_header').
-        with_content(%r{JVMArg "foo".*JVMArg "bar".*JVMArg "baz"}m)
-    end
-  end
+      context 'jvmarg parameter array' do
+        let(:params) { { jvmarg: %w[foo bar baz] } }
 
-  context 'jvmarg parameter string' do
-    let(:params) { { jvmarg: 'bat' } }
+        it 'has multiple jvmarg parameters' do
+          is_expected.to contain_concat__fragment('collectd_plugin_genericjmx_conf_header').
+            with_content(%r{JVMArg "foo".*JVMArg "bar".*JVMArg "baz"}m)
+        end
+      end
 
-    it 'has one jvmarg parameter' do
-      is_expected.to contain_concat__fragment('collectd_plugin_genericjmx_conf_header').with_content(%r{JVMArg "bat"})
-    end
-    it 'has ONLY one jvmarg parameter other than classpath' do
-      is_expected.to contain_concat__fragment('collectd_plugin_genericjmx_conf_header').without_content(%r{(.*JVMArg.*){3,}}m)
-    end
-  end
+      context 'jvmarg parameter string' do
+        let(:params) { { jvmarg: 'bat' } }
 
-  context 'jvmarg parameter empty' do
-    let(:params) { { jvmarg: [] } }
+        it 'has one jvmarg parameter' do
+          is_expected.to contain_concat__fragment('collectd_plugin_genericjmx_conf_header').with_content(%r{JVMArg "bat"})
+        end
+        it 'has ONLY one jvmarg parameter other than classpath' do
+          is_expected.to contain_concat__fragment('collectd_plugin_genericjmx_conf_header').without_content(%r{(.*JVMArg.*){3,}}m)
+        end
+      end
 
-    it 'does not have any jvmarg parameters other than classpath' do
-      is_expected.to contain_concat__fragment('collectd_plugin_genericjmx_conf_header').without_content(%r{(.*JVMArg.*){2,}}m)
+      context 'jvmarg parameter empty' do
+        let(:params) { { jvmarg: [] } }
+
+        it 'does not have any jvmarg parameters other than classpath' do
+          is_expected.to contain_concat__fragment('collectd_plugin_genericjmx_conf_header').without_content(%r{(.*JVMArg.*){2,}}m)
+        end
+      end
     end
   end
 end
