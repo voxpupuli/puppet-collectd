7683b615b34b1a586afad23b018f25334be6ac42
Tim Meusel <tim@bastelfreak.de>
2017-05-26 17:26:01 +0200

    migrate collectd_plugin_filter_rule tests to rspec-puppet-facts

--- spec/defines/collectd_plugin_filter_rule_spec.rb
+++ spec/defines/collectd_plugin_filter_rule_spec.rb
@@ -1,34 +1,30 @@
 require 'spec_helper'
 
 describe 'collectd::plugin::filter::rule', type: :define do
-  let :facts do
-    {
-      osfamily: 'Debian',
-      concat_basedir: '/dne',
-      id: 'root',
-      kernel: 'Linux',
-      path: '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-      collectd_version: '5.0',
-      operatingsystemmajrelease: '7',
-      python_dir: '/usr/local/lib/python2.7/dist-packages'
-    }
-  end
+  on_supported_os.each do |os, facts|
+    context "on #{os} " do
+      let :facts do
+        facts
+      end
 
-  let(:title) { 'MyRule' }
-  let(:params) { { chain: 'MyChain' } }
+      options = os_specific_options(facts)
+      let(:title) { 'MyRule' }
+      let(:params) { { chain: 'MyChain' } }
 
-  context 'Add rule' do
-    it 'create header and footer of rule' do
-      is_expected.to contain_concat__fragment('/etc/collectd/conf.d/filter-chain-MyChain.conf_10_MyRule_0').with(
-        order: '10_MyRule_0',
-        content: '  <Rule "MyRule">',
-        target: '/etc/collectd/conf.d/filter-chain-MyChain.conf'
-      )
-      is_expected.to contain_concat__fragment('/etc/collectd/conf.d/filter-chain-MyChain.conf_10_MyRule_99').with(
-        order: '10_MyRule_99',
-        content: '  </Rule>',
-        target: '/etc/collectd/conf.d/filter-chain-MyChain.conf'
-      )
+      context 'Add rule' do
+        it 'create header and footer of rule' do
+          is_expected.to contain_concat__fragment("#{options[:plugin_conf_dir]}/filter-chain-MyChain.conf_10_MyRule_0").with(
+            order: '10_MyRule_0',
+            content: '  <Rule "MyRule">',
+            target: "#{options[:plugin_conf_dir]}/filter-chain-MyChain.conf"
+          )
+          is_expected.to contain_concat__fragment("#{options[:plugin_conf_dir]}/filter-chain-MyChain.conf_10_MyRule_99").with(
+            order: '10_MyRule_99',
+            content: '  </Rule>',
+            target: "#{options[:plugin_conf_dir]}/filter-chain-MyChain.conf"
+          )
+        end
+      end
     end
   end
 end
