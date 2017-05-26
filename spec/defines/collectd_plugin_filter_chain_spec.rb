02f9e24e29902c0bb65ebe0b73be8fb31785c3c5
Tim Meusel <tim@bastelfreak.de>
2017-05-26 16:10:26 +0200

    migrate collectd_plugin_filter_chain tests to rspec-puppet-facts

--- spec/defines/collectd_plugin_filter_chain_spec.rb
+++ spec/defines/collectd_plugin_filter_chain_spec.rb
@@ -1,73 +1,70 @@
 require 'spec_helper'
 
 describe 'collectd::plugin::filter::chain', type: :define do
-  let :facts do
-    {
-      osfamily: 'Debian',
-      concat_basedir: '/dne',
-      id: 'root',
-      kernel: 'Linux',
-      path: '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-      collectd_version: '5.0',
-      operatingsystemmajrelease: '7',
-      python_dir: '/usr/local/lib/python2.7/dist-packages'
-    }
-  end
 
-  context ':ensure => present and default parameters' do
-    let(:title) { 'MyChain' }
+  on_supported_os.each do |os, facts|
+    context "on #{os} " do
+      let :facts do
+        facts
+      end
 
-    it 'Will create /etc/collectd/conf.d/filter-chain-MyChain.conf' do
-      is_expected.to contain_concat('/etc/collectd/conf.d/filter-chain-MyChain.conf').with(ensure: 'present').
-        that_requires('File[collectd.d]')
-      is_expected.to contain_concat__fragment('/etc/collectd/conf.d/filter-chain-MyChain.conf_MyChain_head').with(
-        order: '00',
-        content: '<Chain "MyChain">',
-        target: '/etc/collectd/conf.d/filter-chain-MyChain.conf'
-      )
-      is_expected.to contain_concat__fragment('/etc/collectd/conf.d/filter-chain-MyChain.conf_MyChain_footer').with(
-        order: '99',
-        content: '</Chain>',
-        target: '/etc/collectd/conf.d/filter-chain-MyChain.conf'
-      )
-    end
-    it { is_expected.not_to contain_collectd__plugin__filter__target('z_chain-MyChain-target') }
-  end
+      options = os_specific_options(facts)
+      context ':ensure => present and default parameters' do
+        let(:title) { 'MyChain' }
 
-  context ':ensure => present and set a default target' do
-    let(:title) { 'MyChain' }
-    let(:params) do
-      {
-        target: 'set',
-        target_options: {
-          'PluginInstance' => 'coretemp',
-          'TypeInstance'   => 'core3'
-        }
-      }
-    end
+        it "Will create #{options[:plugin_conf_dir]}/filter-chain-MyChain.conf" do
+          is_expected.to contain_concat("#{options[:plugin_conf_dir]}/filter-chain-MyChain.conf").with(ensure: 'present').
+            that_requires('File[collectd.d]')
+          is_expected.to contain_concat__fragment("#{options[:plugin_conf_dir]}/filter-chain-MyChain.conf_MyChain_head").with(
+            order: '00',
+            content: '<Chain "MyChain">',
+            target: "#{options[:plugin_conf_dir]}/filter-chain-MyChain.conf"
+          )
+          is_expected.to contain_concat__fragment("#{options[:plugin_conf_dir]}/filter-chain-MyChain.conf_MyChain_footer").with(
+            order: '99',
+            content: '</Chain>',
+            target: "#{options[:plugin_conf_dir]}/filter-chain-MyChain.conf"
+          )
+        end
+        it { is_expected.not_to contain_collectd__plugin__filter__target('z_chain-MyChain-target') }
+      end
 
-    it 'Will add a default target with plugin set and options' do
-      is_expected.to contain_collectd__plugin__filter__target('z_chain-MyChain-target').with(
-        chain: 'MyChain',
-        plugin: 'set',
-        options: {
-          'PluginInstance' => 'coretemp',
-          'TypeInstance'   => 'core3'
-        }
-      )
-    end
-  end
+      context ':ensure => present and set a default target' do
+        let(:title) { 'MyChain' }
+        let(:params) do
+          {
+            target: 'set',
+            target_options: {
+              'PluginInstance' => 'coretemp',
+              'TypeInstance'   => 'core3'
+            }
+          }
+        end
 
-  context ':ensure => absent' do
-    let(:title) { 'MyChain' }
-    let(:params) do
-      {
-        ensure: 'absent'
-      }
-    end
+        it 'Will add a default target with plugin set and options' do
+          is_expected.to contain_collectd__plugin__filter__target('z_chain-MyChain-target').with(
+            chain: 'MyChain',
+            plugin: 'set',
+            options: {
+              'PluginInstance' => 'coretemp',
+              'TypeInstance'   => 'core3'
+            }
+          )
+        end
+      end
 
-    it 'Will remove file /etc/collectd/conf.d/filter-chain-MyChain.conf' do
-      is_expected.to contain_concat('/etc/collectd/conf.d/filter-chain-MyChain.conf').with(ensure: 'absent')
+      context ':ensure => absent' do
+        let(:title) { 'MyChain' }
+        let(:params) do
+          {
+            ensure: 'absent'
+          }
+        end
+
+        it "Will remove file #{options[:plugin_conf_dir]}/filter-chain-MyChain.conf" do
+          is_expected.to contain_concat("#{options[:plugin_conf_dir]}/filter-chain-MyChain.conf").with(ensure: 'absent')
+        end
+      end
     end
   end
 end
