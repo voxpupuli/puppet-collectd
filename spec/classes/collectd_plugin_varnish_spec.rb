56b9b8d8f8e10693236261cf932a6e9474ae147d
Tim Meusel <tim@bastelfreak.de>
2017-05-26 19:01:48 +0200

    migrate collectd_plugin_varnish tests to rspec-puppet-facts

--- spec/classes/collectd_plugin_varnish_spec.rb
+++ spec/classes/collectd_plugin_varnish_spec.rb
@@ -1,96 +1,78 @@
 require 'spec_helper'
 
 describe 'collectd::plugin::varnish', type: :class do
-  let :pre_condition do
-    'include ::collectd'
-  end
+  on_supported_os.each do |os, facts|
+    context "on #{os} " do
+      let :facts do
+        facts
+      end
 
-  context 'When the version is not 5.4' do
-    let :facts do
-      {
-        osfamily: 'RedHat',
-        collectd_version: '5.3',
-        operatingsystemmajrelease: '7',
-        python_dir: '/usr/local/lib/python2.7/dist-packages'
-      }
-    end
-    let :params do
-      {}
-    end
+      let :pre_condition do
+        'include ::collectd'
+      end
 
-    it 'renders the template with the default values' do
+      context 'When the version is not 5.4' do
+        let :facts do
+          facts.merge(collectd_version: '5.3')
+        end
+
+        it 'renders the template with the default values' do
       content = <<EOS
 <Plugin varnish>
   <Instance "localhost">
   </Instance>
 </Plugin>
 EOS
-      is_expected.to contain_collectd__plugin('varnish').with_content(content)
-    end
-  end
-  context 'When the version is nil' do
-    let :facts do
-      {
-        osfamily: 'RedHat',
-        collectd_version: nil,
-        operatingsystemmajrelease: '7',
-        python_dir: '/usr/local/lib/python2.7/dist-packages'
-      }
-    end
-    let :params do
-      {}
-    end
+          is_expected.to contain_collectd__plugin('varnish').with_content(content)
+        end
+      end
+      context 'When the version is nil' do
+        let :facts do
+          facts.merge(collectd_version: nil)
+        end
 
-    it 'renders the template with the default values' do
+        it 'renders the template with the default values' do
       content = <<EOS
 <Plugin varnish>
   <Instance "localhost">
   </Instance>
 </Plugin>
 EOS
-      is_expected.to contain_collectd__plugin('varnish').with_content(content)
-    end
-  end
-
-  context 'When the version is 5.4' do
-    let :facts do
-      {
-        osfamily: 'RedHat',
-        collectd_version: '5.4',
-        operatingsystemmajrelease: '7',
-        python_dir: '/usr/local/lib/python2.7/dist-packages'
-      }
-    end
-
-    context 'when there are no params given' do
-      let :params do
-        {}
+          is_expected.to contain_collectd__plugin('varnish').with_content(content)
+        end
       end
 
-      it 'renders the template with the default values' do
-        content = <<EOS
+      context 'When the version is 5.4' do
+        let :facts do
+          facts.merge(collectd_version: '5.4')
+        end
+
+        context 'when there are no params given' do
+
+          it 'renders the template with the default values' do
+            content = <<EOS
 <Plugin varnish>
   <Instance "localhost">
   </Instance>
 </Plugin>
 EOS
-        is_expected.to contain_collectd__plugin('varnish').with_content(content)
+          is_expected.to contain_collectd__plugin('varnish').with_content(content)
+        end
       end
-    end
-    context 'when there are params given' do
-      let :params do
-        {
-          'instances' => {
-            'warble' => {
-              'BATMAN' => true,
-              'Robin' => false
+      context 'when there are params given' do
+        let :params do
+          {
+            'instances' => {
+              'warble' => {
+                'BATMAN' => true,
+                'Robin' => false
+              }
             }
           }
-        }
-      end
+        end
 
-      it 'renders the template with the values passed in the params' do
-        content = <<EOS
+        it 'renders the template with the values passed in the params' do
+          content = <<EOS
 <Plugin varnish>
   <Instance "warble">
     BATMAN true
@@ -98,7 +80,9 @@ EOS
   </Instance>
 </Plugin>
 EOS
-        is_expected.to contain_collectd__plugin('varnish').with_content(content)
+            is_expected.to contain_collectd__plugin('varnish').with_content(content)
+          end
+        end
       end
     end
   end
