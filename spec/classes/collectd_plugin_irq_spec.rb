f389dcfdefad797aa8cc6fd7bdeae3b3ebe8bd90
Tim Meusel <tim@bastelfreak.de>
2017-05-26 19:14:41 +0200

    migrate collectd_plugin_irq tests to rspec-puppet-facts

--- spec/classes/collectd_plugin_irq_spec.rb
+++ spec/classes/collectd_plugin_irq_spec.rb
@@ -1,45 +1,50 @@
 require 'spec_helper'
 
 describe 'collectd::plugin::irq', type: :class do
-  let :facts do
-    {
-      osfamily: 'RedHat',
-      collectd_version: '4.8.0',
-      operatingsystemmajrelease: '7',
-      python_dir: '/usr/local/lib/python2.7/dist-packages'
-    }
-  end
+  on_supported_os.each do |os, facts|
+    context "on #{os} " do
+      let :facts do
+        facts
+      end
 
-  context ':ensure => present and :irqs => [90,91,92]' do
-    let :params do
-      { irqs: [90, 91, 92] }
-    end
+      options = os_specific_options(facts)
 
-    it 'Will create /etc/collectd.d/10-irq.conf' do
-      is_expected.to contain_file('irq.load').with(ensure: 'present',
-                                                   path: '/etc/collectd.d/10-irq.conf',
-                                                   content: %r{Irq  \"90\"\n.+Irq  \"91\"\n.+Irq  \"92\"}m)
-    end
-  end
+      context ':ensure => present and :irqs => [90,91,92]' do
+        let :params do
+          { irqs: [90, 91, 92] }
+        end
 
-  context ':ensure => absent' do
-    let :params do
-      { irqs: [90, 91, 92], ensure: 'absent' }
-    end
+        it "Will create #{options[:plugin_conf_dir]}/10-irq.conf" do
+          is_expected.to contain_file('irq.load').with(
+            ensure: 'present',
+            path: "#{options[:plugin_conf_dir]}/10-irq.conf",
+            content: %r{Irq  \"90\"\n.+Irq  \"91\"\n.+Irq  \"92\"}m
+          )
+        end
+      end
 
-    it 'Will not create /etc/collectd.d/10-irq.conf' do
-      is_expected.to contain_file('irq.load').with(ensure: 'absent',
-                                                   path: '/etc/collectd.d/10-irq.conf')
-    end
-  end
+      context ':ensure => absent' do
+        let :params do
+          { irqs: [90, 91, 92], ensure: 'absent' }
+        end
 
-  context ':disks is not an array' do
-    let :params do
-      { irqs: '90,91,92' }
-    end
+        it 'Will not create /etc/collectd.d/10-irq.conf' do
+          is_expected.to contain_file('irq.load').with(
+            ensure: 'absent',
+            path: "#{options[:plugin_conf_dir]}/10-irq.conf"
+          )
+        end
+      end
 
-    it 'Will raise an error about :irqs being a String' do
-      is_expected.to compile.and_raise_error(%r{String})
+      context ':disks is not an array' do
+        let :params do
+          { irqs: '90,91,92' }
+        end
+
+        it 'Will raise an error about :irqs being a String' do
+          is_expected.to compile.and_raise_error(%r{String})
+        end
+      end
     end
   end
 end
