45c1e9c8282ab0737f319329df7d97d118a9affb
Tim Meusel <tim@bastelfreak.de>
2017-05-27 15:26:18 +0200

    migrate collectd_plugin_oracle tests to rspec-puppet-facts

--- spec/classes/collectd_plugin_oracle_spec.rb
+++ spec/classes/collectd_plugin_oracle_spec.rb
@@ -1,46 +1,55 @@
 require 'spec_helper'
 
 describe 'collectd::plugin::oracle', type: :class do
-  let :facts do
-    {
-      osfamily: 'RedHat',
-      collectd_version: '4.8.0',
-      operatingsystemmajrelease: '7',
-      python_dir: '/usr/local/lib/python2.7/dist-packages'
-    }
-  end
+  on_supported_os.each do |os, facts|
+    context "on #{os} " do
+      let :facts do
+        facts
+      end
 
-  let(:config_filename) { '/etc/collectd.d/15-oracle.conf' }
+      options = os_specific_options(facts)
 
-  context ':ensure => present, default host and port' do
-    it 'Will create /etc/collectd.d/10-oracle.conf' do
-      is_expected.to contain_file('oracle.load').with(ensure: 'present',
-                                                      path: '/etc/collectd.d/10-oracle.conf',
-                                                      content: %r{LoadPlugin oracle})
-    end
+      let(:config_filename) { "#{options[:plugin_conf_dir]}/15-oracle.conf" }
 
-    it 'Will create /etc/collectd.d/15-oracle.conf' do
-      is_expected.to contain_concat(config_filename).with(ensure: 'present',
-                                                          path: config_filename)
-    end
+      context ':ensure => present, default host and port' do
+        it "Will create #{options[:plugin_conf_dir]}/10-oracle.conf" do
+          is_expected.to contain_file('oracle.load').with(
+            ensure: 'present',
+            path: "#{options[:plugin_conf_dir]}/10-oracle.conf",
+            content: %r{LoadPlugin oracle}
+          )
+        end
 
-    it { is_expected.to contain_concat__fragment('collectd_plugin_oracle_conf_header').with_content(%r{<Plugin oracle>}) }
-    it { is_expected.to contain_concat__fragment('collectd_plugin_oracle_conf_footer').with_content(%r{</Plugin>}) }
-  end
+        it "Will create #{options[:plugin_conf_dir]}/15-oracle.conf" do
+          is_expected.to contain_concat(config_filename).with(
+            ensure: 'present',
+            path: config_filename
+          )
+        end
 
-  context ':ensure => absent' do
-    let :params do
-      { ensure: 'absent' }
-    end
+        it { is_expected.to contain_concat__fragment('collectd_plugin_oracle_conf_header').with_content(%r{<Plugin oracle>}) }
+        it { is_expected.to contain_concat__fragment('collectd_plugin_oracle_conf_footer').with_content(%r{</Plugin>}) }
+      end
 
-    it 'Will not create /etc/collectd.d/10-oracle.conf' do
-      is_expected.to contain_file('oracle.load').with(ensure: 'absent',
-                                                      path: '/etc/collectd.d/10-oracle.conf')
-    end
+      context ':ensure => absent' do
+        let :params do
+          { ensure: 'absent' }
+        end
 
-    it 'Will not create /etc/collectd.d/15-oracle.conf' do
-      is_expected.to contain_concat(config_filename).with(ensure: 'absent',
-                                                          path: config_filename)
+        it "Will not create #{options[:plugin_conf_dir]}/10-oracle.conf" do
+          is_expected.to contain_file('oracle.load').with(
+            ensure: 'absent',
+            path: "#{options[:plugin_conf_dir]}/10-oracle.conf"
+          )
+        end
+
+        it "Will not create #{options[:plugin_conf_dir]}/15-oracle.conf" do
+          is_expected.to contain_concat(config_filename).with(
+            ensure: 'absent',
+            path: config_filename
+          )
+        end
+      end
     end
   end
 end
