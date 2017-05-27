25e95d79c8b9357c712074f41f62d9a7827af5dd
Tim Meusel <tim@bastelfreak.de>
2017-05-27 18:02:15 +0200

    migrate collectd_plugin_snmp tests to rspec-puppet-facts

--- spec/classes/collectd_plugin_snmp_spec.rb
+++ spec/classes/collectd_plugin_snmp_spec.rb
@@ -1,76 +1,80 @@
 require 'spec_helper'
 
 describe 'collectd::plugin::snmp', type: :class do
-  let :facts do
-    {
-      osfamily: 'RedHat',
-      collectd_version: '4.8.0',
-      operatingsystemmajrelease: '7',
-      python_dir: '/usr/local/lib/python2.7/dist-packages'
-    }
-  end
+  on_supported_os.each do |os, facts|
+    context "on #{os} " do
+      let :facts do
+        facts
+      end
 
-  let :pre_condition do
-    'include ::collectd'
-  end
+      options = os_specific_options(facts)
+      let :pre_condition do
+        'include ::collectd'
+      end
 
-  context ':ensure => present and dataset for AMAVIS-MIB::inMsgs.0' do
-    let :params do
-      {
-        data: {
-          'amavis_incoming_messages' => {
-            'type'     => 'counter',
-            'table'    => false,
-            'instance' => 'amavis.inMsgs',
-            'values'   => ['AMAVIS-MIB::inMsgs.0']
+      context ':ensure => present and dataset for AMAVIS-MIB::inMsgs.0' do
+        let :params do
+          {
+            data: {
+              'amavis_incoming_messages' => {
+                'type'     => 'counter',
+                'table'    => false,
+                'instance' => 'amavis.inMsgs',
+                'values'   => ['AMAVIS-MIB::inMsgs.0']
+              }
+            },
+            hosts: {
+              'scan04' => {
+                'address'   => '127.0.0.1',
+                'version'   => 2,
+                'community' => 'public',
+                'collect'   => ['amavis_incoming_messages'],
+                'interval'  => 10
+              }
+            }
           }
-        },
-        hosts: {
-          'scan04' => {
-            'address'   => '127.0.0.1',
-            'version'   => 2,
-            'community' => 'public',
-            'collect'   => ['amavis_incoming_messages'],
-            'interval'  => 10
-          }
-        }
-      }
-    end
+        end
 
-    it 'Will create /etc/collectd.d/10-snmp.conf' do
-      is_expected.to contain_file('snmp.load').with(ensure: 'present',
-                                                    path: '/etc/collectd.d/10-snmp.conf',
-                                                    content: %r{Data "amavis_incoming_messages".+Instance "amavis.inMsgs".+Host "scan04".+Community "public"}m)
-    end
-  end
+        it "Will create #{options[:plugin_conf_dir]}/10-snmp.conf" do
+          is_expected.to contain_file('snmp.load').with(
+            ensure: 'present',
+            path: "#{options[:plugin_conf_dir]}/10-snmp.conf",
+            content: %r{Data "amavis_incoming_messages".+Instance "amavis.inMsgs".+Host "scan04".+Community "public"}m
+          )
+        end
+      end
 
-  context ':ensure => absent' do
-    let :params do
-      {
-        ensure: 'absent',
-        data: {
-          'amavis_incoming_messages' => {
-            'type'     => 'counter',
-            'table'    => false,
-            'instance' => 'amavis.inMsgs',
-            'values'   => ['AMAVIS-MIB::inMsgs.0']
+      context ':ensure => absent' do
+        let :params do
+          {
+            ensure: 'absent',
+            data: {
+              'amavis_incoming_messages' => {
+                'type'     => 'counter',
+                'table'    => false,
+                'instance' => 'amavis.inMsgs',
+                'values'   => ['AMAVIS-MIB::inMsgs.0']
+              }
+            },
+            hosts: {
+              'scan04' => {
+                'address'   => '127.0.0.1',
+                'version'   => 2,
+                'community' => 'public',
+                'collect'   => ['amavis_incoming_messages'],
+                'interval'  => 10
+              }
+            }
           }
-        },
-        hosts: {
-          'scan04' => {
-            'address'   => '127.0.0.1',
-            'version'   => 2,
-            'community' => 'public',
-            'collect'   => ['amavis_incoming_messages'],
-            'interval'  => 10
-          }
-        }
-      }
-    end
+        end
 
-    it 'Will not create /etc/collectd.d/10-snmp.conf' do
-      is_expected.to contain_file('snmp.load').with(ensure: 'absent',
-                                                    path: '/etc/collectd.d/10-snmp.conf')
+        it "Will not create #{options[:plugin_conf_dir]}/10-snmp.conf" do
+          is_expected.to contain_file('snmp.load').with(
+            ensure: 'absent',
+            path: "#{options[:plugin_conf_dir]}/10-snmp.conf"
+          )
+        end
+      end
     end
   end
 end
