2af67a40a6a68119c408bccec98fc15048eafc5f
Tim Meusel <tim@bastelfreak.de>
2017-05-27 15:05:53 +0200

    migrate collectd_plugin_openldap tests to rspec-puppet-facts

--- spec/classes/collectd_plugin_openldap_spec.rb
+++ spec/classes/collectd_plugin_openldap_spec.rb
@@ -1,72 +1,49 @@
 require 'spec_helper'
 
 describe 'collectd::plugin::openldap', type: :class do
-  let :pre_condition do
-    'include ::collectd'
-  end
-  ######################################################################
-  # Default param validation, compilation succeeds
-  let :facts do
-    {
-      osfamily: 'RedHat',
-      collectd_version: '4.8.0',
-      operatingsystemmajrelease: '7',
-      python_dir: '/usr/local/lib/python2.7/dist-packages'
-    }
-  end
+  on_supported_os.each do |os, facts|
+    context "on #{os} " do
+      let :facts do
+        facts
+      end
 
-  context ':ensure => present, default params' do
-    let :facts do
-      {
-        osfamily: 'RedHat',
-        collectd_version: '5.4',
-        operatingsystemmajrelease: '7',
-        python_dir: '/usr/local/lib/python2.7/dist-packages'
-      }
-    end
-    let :params do
-      {}
-    end
+      options = os_specific_options(facts)
+      let :pre_condition do
+        'include ::collectd'
+      end
 
-    it 'Will create /etc/collectd.d/10-openldap.conf' do
-      content = <<EOS
+      context ':ensure => present, default params' do
+
+        it "Will create #{options[:plugin_conf_dir]}/10-openldap.conf" do
+          content = <<EOS
 <Plugin "openldap">
   <Instance "localhost">
     URL "ldap://localhost/"
   </Instance>
 </Plugin>
 EOS
-      is_expected.to contain_collectd__plugin('openldap').with_content(content)
-    end
-  end
+          is_expected.to contain_collectd__plugin('openldap').with_content(content)
+        end
+      end
 
-  context ':instances param is a hash' do
-    let :facts do
-      {
-        osfamily: 'RedHat',
-        collectd_version: '5.4',
-        operatingsystemmajrelease: '7',
-        python_dir: '/usr/local/lib/python2.7/dist-packages'
-      }
-    end
-
-    let :params do
-      {
-        instances: {
-          'ldap1' => {
-            'url' => 'ldap://ldap1.example.com'
-          },
-          'ldap2' => {
-            'url' => 'ldap://ldap2.example.com',
-            'binddn'   => 'cn=Monitor',
-            'password' => 'password'
+      context ':instances param is a hash' do
+        let :params do
+          {
+            instances: {
+              'ldap1' => {
+                'url' => 'ldap://ldap1.example.com'
+              },
+              'ldap2' => {
+                'url' => 'ldap://ldap2.example.com',
+                'binddn'   => 'cn=Monitor',
+                'password' => 'password'
+              }
+            }
           }
-        }
-      }
-    end
+        end
 
-    it 'Will create /etc/collectd.d/10-openldap.conf with two :instances params' do
-      content = <<EOS
+        it "Will create #{options[:plugin_conf_dir]}/10-openldap.conf with two :instances params" do
+          content = <<EOS
 <Plugin "openldap">
   <Instance "ldap1">
     URL "ldap://ldap1.example.com"
@@ -78,53 +55,36 @@ EOS
   </Instance>
 </Plugin>
 EOS
-      is_expected.to contain_collectd__plugin('openldap').with_content(content)
-    end
-  end
+          is_expected.to contain_collectd__plugin('openldap').with_content(content)
+        end
+      end
 
-  ######################################################################
-  # Remaining parameter validation, compilation fails
+      context ':interval is not default and is an integer' do
+        let :params do
+          { interval: 15 }
+        end
 
-  context ':interval is not default and is an integer' do
-    let :facts do
-      {
-        osfamily: 'RedHat',
-        collectd_version: '5.4',
-        operatingsystemmajrelease: '7',
-        python_dir: '/usr/local/lib/python2.7/dist-packages'
-      }
-    end
-    let :params do
-      { interval: 15 }
-    end
+        it "Will create #{options[:plugin_conf_dir]}/10-openldap.conf" do
+          is_expected.to contain_file('openldap.load').with(
+            ensure: 'present',
+            path: "#{options[:plugin_conf_dir]}/10-openldap.conf",
+            content: %r{^  Interval 15}
+          )
+        end
+      end
 
-    it 'Will create /etc/collectd.d/10-openldap.conf' do
-      is_expected.to contain_file('openldap.load').with(
-        ensure: 'present',
-        path: '/etc/collectd.d/10-openldap.conf',
-        content: %r{^  Interval 15}
-      )
-    end
-  end
+      context ':ensure => absent' do
+        let :params do
+          { ensure: 'absent' }
+        end
 
-  context ':ensure => absent' do
-    let :facts do
-      {
-        osfamily: 'RedHat',
-        collectd_version: '5.4',
-        operatingsystemmajrelease: '7',
-        python_dir: '/usr/local/lib/python2.7/dist-packages'
-      }
-    end
-    let :params do
-      { ensure: 'absent' }
-    end
-
-    it 'Will not create /etc/collectd.d/10-openldap.conf' do
-      is_expected.to contain_file('openldap.load').with(
-        ensure: 'absent',
-        path: '/etc/collectd.d/10-openldap.conf'
-      )
+        it "Will not create #{options[:plugin_conf_dir]}/10-openldap.conf" do
+          is_expected.to contain_file('openldap.load').with(
+            ensure: 'absent',
+            path: "#{options[:plugin_conf_dir]}/10-openldap.conf"
+          )
+        end
+      end
     end
   end
 end
