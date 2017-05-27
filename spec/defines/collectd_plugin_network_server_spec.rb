e64f980d83cea688fe877a91e5db6b9a9d52b6eb
Tim Meusel <tim@bastelfreak.de>
2017-05-28 00:11:21 +0200

    migrate collectd_plugin_network_server_spec tests to rspec-puppet-facts

--- spec/defines/collectd_plugin_network_server_spec.rb
+++ spec/defines/collectd_plugin_network_server_spec.rb
@@ -1,52 +1,46 @@
 require 'spec_helper'
 
 describe 'collectd::plugin::network::server', type: :define do
-  context ':ensure => present, create one complex server, collectd 5.1.0' do
-    let :facts do
-      {
-        osfamily: 'RedHat',
-        collectd_version: '5.1.0',
-        operatingsystemmajrelease: '7',
-        python_dir: '/usr/local/lib/python2.7/dist-packages'
-      }
-    end
-    let(:title) { 'node1' }
-    let :params do
-      {
-        port: '1234',
-        interface: 'eth0',
-        securitylevel: 'Encrypt',
-        username: 'foo',
-        password: 'bar'
-      }
-    end
+  on_supported_os.each do |os, facts|
+    context "on #{os} " do
+      let :facts do
+        facts
+      end
 
-    it 'Will create /etc/collectd.d/network-server-node1.conf for collectd >= 4.7' do
-      is_expected.to contain_file('/etc/collectd.d/network-server-node1.conf').
-        with(ensure: 'present',
-             path: '/etc/collectd.d/network-server-node1.conf',
-             content: "<Plugin network>\n  <Server \"node1\" \"1234\">\n    SecurityLevel \"Encrypt\"\n    Username \"foo\"\n    Password \"bar\"\n    Interface \"eth0\"\n\n  </Server>\n</Plugin>\n")
-    end
-  end
+      options = os_specific_options(facts)
+      context ':ensure => present, create one complex server' do
+        let(:title) { 'node1' }
+        let :params do
+          {
+            port: '1234',
+            interface: 'eth0',
+            securitylevel: 'Encrypt',
+            username: 'foo',
+            password: 'bar'
+          }
+        end
 
-  context ':ensure => absent' do
-    let :facts do
-      {
-        osfamily: 'RedHat',
-        collectd_version: '5.1.0',
-        operatingsystemmajrelease: '7',
-        python_dir: '/usr/local/lib/python2.7/dist-packages'
-      }
-    end
-    let(:title) { 'node1' }
-    let :params do
-      {
-        ensure: 'absent'
-      }
-    end
+        it "Will create #{options[:plugin_conf_dir]}/network-server-node1.conf for collectd >= 4.7" do
+          is_expected.to contain_file("#{options[:plugin_conf_dir]}/network-server-node1.conf").with(
+            ensure: 'present',
+            path: "#{options[:plugin_conf_dir]}/network-server-node1.conf",
+            content: "<Plugin network>\n  <Server \"node1\" \"1234\">\n    SecurityLevel \"Encrypt\"\n    Username \"foo\"\n    Password \"bar\"\n    Interface \"eth0\"\n\n  </Server>\n</Plugin>\n"
+          )
+        end
+      end
 
-    it 'Will not create /etc/collectd.d/network-server-node1.conf' do
-      is_expected.to contain_file('/etc/collectd.d/network-server-node1.conf').with(ensure: 'absent')
+      context ':ensure => absent' do
+        let(:title) { 'node1' }
+        let :params do
+          {
+            ensure: 'absent'
+          }
+        end
+
+        it "Will not create #{options[:plugin_conf_dir]}/network-server-node1.conf" do
+          is_expected.to contain_file("#{options[:plugin_conf_dir]}/network-server-node1.conf").with(ensure: 'absent')
+        end
+      end
     end
   end
 end
