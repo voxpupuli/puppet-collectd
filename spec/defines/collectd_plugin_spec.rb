108cb90d3921f1dd946227e9b4d99d2f834dbcdc
Tim Meusel <tim@bastelfreak.de>
2017-05-27 19:18:48 +0200

    migrate collectd_plugin tests to rspec-puppet-facts

--- spec/defines/collectd_plugin_spec.rb
+++ spec/defines/collectd_plugin_spec.rb
@@ -1,35 +1,34 @@
 require 'spec_helper'
 
 describe 'collectd::plugin', type: :define do
-  context 'loading a plugin on collectd <= 4.9.4' do
-    let(:title) { 'test' }
-    let :facts do
-      {
-        collectd_version: '5.3',
-        osfamily: 'Debian',
-        operatingsystemmajrelease: '7',
-        python_dir: '/usr/local/lib/python2.7/dist-packages'
-      }
-    end
+  on_supported_os.each do |os, facts|
+    context "on #{os} " do
+      let :facts do
+        facts
+      end
 
-    it 'Will create /etc/collectd/conf.d/10-test.conf with the LoadPlugin syntax with brackets' do
-      is_expected.to contain_file('test.load').with_content(%r{<LoadPlugin})
-    end
-  end
+      options = os_specific_options(facts)
+      context 'loading a plugin on collectd <= 4.9.4' do
+        let(:title) { 'test' }
+        let :facts do
+          facts.merge(collectd_version: '5.3')
+        end
 
-  context 'loading a plugin on collectd => 4.9.3' do
-    let(:title) { 'test' }
-    let :facts do
-      {
-        collectd_version: '4.9.3',
-        osfamily: 'Debian',
-        operatingsystemmajrelease: '7',
-        python_dir: '/usr/local/lib/python2.7/dist-packages'
-      }
-    end
+        it "Will create #{options[:plugin_conf_dir]}/10-test.conf with the LoadPlugin syntax with brackets" do
+          is_expected.to contain_file('test.load').with_content(%r{<LoadPlugin})
+        end
+      end
 
-    it 'Will create /etc/collectd/conf.d/10-test.conf with the LoadPlugin syntax without brackets' do
-      is_expected.to contain_file('test.load').without_content(%r{<LoadPlugin})
+      context 'loading a plugin on collectd => 4.9.3' do
+        let(:title) { 'test' }
+        let :facts do
+          facts.merge(collectd_version: '4.9.3')
+        end
+
+        it "Will create #{options[:plugin_conf_dir]}conf.d/10-test.conf with the LoadPlugin syntax without brackets" do
+          is_expected.to contain_file('test.load').without_content(%r{<LoadPlugin})
+        end
+      end
     end
   end
 end
