8b93bee196e874c92c9a9334ed1d8df1b3304656
Tim Meusel <tim@bastelfreak.de>
2017-05-27 23:56:52 +0200

    migrate collectd_plugin_write_log tests to rspec-puppet-facts

--- spec/classes/collectd_plugin_write_log_spec.rb
+++ spec/classes/collectd_plugin_write_log_spec.rb
@@ -4,7 +4,7 @@ describe 'collectd::plugin::write_log', type: :class do
   on_supported_os.each do |os, facts|
     context "on #{os} " do
       let :facts do
-        facts.merge
+        facts
       end
 
       options = os_specific_options(facts)
