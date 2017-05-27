b2a89501d91c05a265f65cbb2c3512861ed0ba83
Tim Meusel <tim@bastelfreak.de>
2017-05-27 18:48:14 +0200

    migrate collectd_plugin_write_prometheus tests to rspec-puppet-facts

--- spec/classes/collectd_plugin_write_prometheus_spec.rb
+++ spec/classes/collectd_plugin_write_prometheus_spec.rb
@@ -1,35 +1,39 @@
 require 'spec_helper'
 
 describe 'collectd::plugin::write_prometheus', type: :class do
-  let :facts do
-    {
-      osfamily: 'RedHat',
-      collectd_version: '5.7.0',
-      operatingsystemmajrelease: '7',
-      python_dir: '/usr/local/lib/python2.7/dist-packages'
-    }
-  end
+  on_supported_os.each do |os, facts|
+    context "on #{os} " do
+      let :facts do
+        facts.merge(collectd_version: '5.7')
+      end
 
-  context ':ensure => present and :port => \'9103\'' do
-    let :params do
-      { port: '9103' }
-    end
+      options = os_specific_options(facts)
+      context ':ensure => present and :port => "9103"' do
+        let :params do
+          { port: '9103' }
+        end
 
-    it 'Will create /etc/collectd.d/10-write_prometheus.conf' do
-      is_expected.to contain_file('write_prometheus.load').with(ensure: 'present')
-      is_expected.to contain_file('write_prometheus.load').with(path: '/etc/collectd.d/10-write_prometheus.conf')
-      is_expected.to contain_file('write_prometheus.load').with(content: %r{Port "9103"})
-    end
-  end
+        it "Will create #{options[:plugin_conf_dir]}/10-write_prometheus.conf" do
+          is_expected.to contain_file('write_prometheus.load').with(
+            ensure: 'present',
+            path: "#{options[:plugin_conf_dir]}/10-write_prometheus.conf",
+            content: %r{Port "9103"},
+          )
+        end
+      end
 
-  context ':ensure => absent' do
-    let :params do
-      { ensure: 'absent' }
-    end
+      context ':ensure => absent' do
+        let :params do
+          { ensure: 'absent' }
+        end
 
-    it 'Will not create ' do
-      is_expected.to contain_file('write_prometheus.load').with(ensure: 'absent',
-                                                                path: '/etc/collectd.d/10-write_prometheus.conf')
+        it 'Will not create ' do
+          is_expected.to contain_file('write_prometheus.load').with(
+            ensure: 'absent',
+            path: "#{options[:plugin_conf_dir]}/10-write_prometheus.conf"
+          )
+        end
+      end
     end
   end
 end
