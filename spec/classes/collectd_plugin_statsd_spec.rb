566e5ecfc383097a5fc68ce25eb00322c4582ed6
Tim Meusel <tim@bastelfreak.de>
2017-05-27 18:09:20 +0200

    migrate collectd_plugin_statsd tests to rspec-puppet-facts

--- spec/classes/collectd_plugin_statsd_spec.rb
+++ spec/classes/collectd_plugin_statsd_spec.rb
@@ -1,70 +1,78 @@
 require 'spec_helper'
 
 describe 'collectd::plugin::statsd', type: :class do
-  let :facts do
-    {
-      osfamily: 'RedHat',
-      collectd_version: '4.8.0',
-      operatingsystemmajrelease: '7',
-      python_dir: '/usr/local/lib/python2.7/dist-packages'
-    }
-  end
-
-  let :pre_condition do
-    'include ::collectd'
-  end
-
-  context ':ensure => present' do
-    context ':ensure => present and default parameters' do
-      it 'Will create /etc/collectd.d/10-statsd.conf' do
-        is_expected.to contain_file('statsd.load').with(ensure: 'present',
-                                                        path: '/etc/collectd.d/10-statsd.conf',
-                                                        content: %r{<Plugin statsd>\n</Plugin>})
-      end
-    end
-
-    context ':ensure => present and hostname and port' do
-      let :params do
-        {
-          ensure: 'present',
-          host: '192.0.0.1',
-          port: '9876'
-        }
+  on_supported_os.each do |os, facts|
+    context "on #{os} " do
+      let :facts do
+        facts
       end
 
-      it 'Will create /etc/collectd.d/10-statsd.conf' do
-        is_expected.to contain_file('statsd.load').with(ensure: 'present',
-                                                        path: '/etc/collectd.d/10-statsd.conf',
-                                                        content: %r{Host "192.0.0.1".+Port 9876}m)
-      end
-    end
-
-    context ':ensure => present and countersum' do
-      let :params do
-        {
-          ensure: 'present',
-          countersum: true
-        }
+      options = os_specific_options(facts)
+      let :pre_condition do
+        'include ::collectd'
       end
 
-      it 'Will create /etc/collectd.d/10-statsd.conf' do
-        is_expected.to contain_file('statsd.load').with(ensure: 'present',
-                                                        path: '/etc/collectd.d/10-statsd.conf',
-                                                        content: %r{CounterSum true}m)
+      context ':ensure => present' do
+        context ':ensure => present and default parameters' do
+          it 'Will create /etc/collectd.d/10-statsd.conf' do
+            is_expected.to contain_file('statsd.load').with(
+              ensure: 'present',
+              path: "#{options[:plugin_conf_dir]}/10-statsd.conf",
+              content: %r{<Plugin statsd>\n</Plugin>}
+            )
+          end
+        end
+
+        context ':ensure => present and hostname and port' do
+          let :params do
+            {
+              ensure: 'present',
+              host: '192.0.0.1',
+              port: '9876'
+            }
+          end
+
+          it "Will create #{options[:plugin_conf_dir]}/10-statsd.conf" do
+            is_expected.to contain_file('statsd.load').with(
+              ensure: 'present',
+              path: "#{options[:plugin_conf_dir]}/10-statsd.conf",
+              content: %r{Host "192.0.0.1".+Port 9876}m
+            )
+          end
+        end
+
+        context ':ensure => present and countersum' do
+          let :params do
+            {
+              ensure: 'present',
+              countersum: true
+            }
+          end
+
+          it "Will create #{options[:plugin_conf_dir]}/10-statsd.conf" do
+            is_expected.to contain_file('statsd.load').with(
+              ensure: 'present',
+              path: "#{options[:plugin_conf_dir]}/10-statsd.conf",
+              content: %r{CounterSum true}m
+            )
+          end
+        end
       end
-    end
-  end
 
-  context ':ensure => absent' do
-    let :params do
-      {
-        ensure: 'absent'
-      }
-    end
+      context ':ensure => absent' do
+        let :params do
+          {
+            ensure: 'absent'
+          }
+        end
 
-    it 'Will not create /etc/collectd.d/10-statsd.conf' do
-      is_expected.to contain_file('statsd.load').with(ensure: 'absent',
-                                                      path: '/etc/collectd.d/10-statsd.conf')
+        it "Will not create #{options[:plugin_conf_dir]}/10-statsd.conf" do
+          is_expected.to contain_file('statsd.load').with(
+            ensure: 'absent',
+            path: "#{options[:plugin_conf_dir]}/10-statsd.conf"
+          )
+        end
+      end
     end
   end
 end
