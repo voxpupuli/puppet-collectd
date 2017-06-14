92a286b5abe516e6e320bfd07bf4434f1b4681b9
Wildente <wildente@users.noreply.github.com>
2017-06-14 10:09:25 +0200

    add fscache plugin
    https://collectd.org/wiki/index.php/Plugin:FSCache

---
+++ manifests/plugin/fscache.pp
@@ -0,0 +1,11 @@
+# https://collectd.org/wiki/index.php/Plugin:FSCache
+class collectd::plugin::fscache (
+  $ensure = 'present',
+) {
+
+  include ::collectd
+
+  collectd::plugin { 'fscache':
+    ensure => $ensure,
+  }
+}
