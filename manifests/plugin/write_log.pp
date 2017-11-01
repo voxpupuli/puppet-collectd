a708baa3103a397f1a5dc2d79eb04dbfe5dcadb6
Tim Meusel <tim@bastelfreak.de>
2017-11-01 15:26:53 +0100

    replace validate_* with datatypes in write_log

--- manifests/plugin/write_log.pp
+++ manifests/plugin/write_log.pp
@@ -1,11 +1,9 @@
 class collectd::plugin::write_log (
-  $format   = 'JSON',
-  $ensure   = 'present',
+  String $format = 'JSON',
+  $ensure        = 'present',
 ) {
 
   include ::collectd
-
-  validate_string($format)
 
   collectd::plugin { 'write_log':
     ensure  => $ensure,
