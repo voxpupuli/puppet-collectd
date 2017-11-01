20bc846aabfcd83a94290e93870f4204db05b293
Tim Meusel <tim@bastelfreak.de>
2017-11-01 13:11:06 +0100

    replace validate_* with datatypes in apache

--- manifests/plugin/apache.pp
+++ manifests/plugin/apache.pp
@@ -1,21 +1,15 @@
 # https://collectd.org/wiki/index.php/Plugin:Apache
 class collectd::plugin::apache (
-  $ensure                  = 'present',
-  $manage_package          = undef,
-  $instances               = { 'localhost' => { 'url' => 'http://localhost/mod_status?auto' } },
-  $interval                = undef,
-  $package_install_options = $collectd::package_install_options,
+  $ensure                                  = 'present',
+  $manage_package                          = undef,
+  Hash $instances                          = { 'localhost' => { 'url' => 'http://localhost/mod_status?auto' } },
+  $interval                                = undef,
+  Optional[Array] $package_install_options = $collectd::package_install_options,
 ) {
 
   include ::collectd
 
   $_manage_package = pick($manage_package, $::collectd::manage_package)
-
-  validate_hash($instances)
-
-  if $package_install_options != undef {
-    validate_array($package_install_options)
-  }
 
   if $::osfamily == 'RedHat' {
     if $_manage_package {
