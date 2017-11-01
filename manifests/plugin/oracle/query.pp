8cfb9cd292ef78e1452deeb8dcd68008959adb11
Tim Meusel <tim@bastelfreak.de>
2017-11-01 13:09:45 +0100

    replace validate_* with datatypes in query

--- manifests/plugin/oracle/query.pp
+++ manifests/plugin/oracle/query.pp
@@ -1,16 +1,12 @@
 # https://collectd.org/wiki/index.php/Plugin:Oracle
 define collectd::plugin::oracle::query (
-  $statement,
-  $results,
-  $query = $name,
+  String $statement,
+  Array $results,
+  String $query = $name,
 ) {
 
   include ::collectd
   include ::collectd::plugin::oracle
-
-  validate_string($query)
-  validate_string($statement)
-  validate_array($results)
 
   concat::fragment { "collectd_plugin_oracle_query_${name}":
     order   => '10',
