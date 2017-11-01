06efa17e13367379669c1520a6bfaef2863c9d02
Tim Meusel <tim@bastelfreak.de>
2017-11-01 14:43:56 +0100

    replace validate_* with datatypes in write_graphite::carbon

--- manifests/plugin/write_graphite/carbon.pp
+++ manifests/plugin/write_graphite/carbon.pp
@@ -1,26 +1,21 @@
 # a single graphite backend
 define collectd::plugin::write_graphite::carbon (
-  $ensure            = 'present',
-  $graphitehost      = 'localhost',
-  $graphiteport      = 2003,
-  $storerates        = true,
-  $graphiteprefix    = 'collectd.',
-  $graphitepostfix   = undef,
-  $interval          = undef,
-  $escapecharacter   = '_',
-  $alwaysappendds    = false,
-  $protocol          = 'tcp',
-  $separateinstances = false,
-  $logsenderrors     = true,
+  $ensure                    = 'present',
+  $graphitehost              = 'localhost',
+  $graphiteport              = 2003,
+  Boolean $storerates        = true,
+  $graphiteprefix            = 'collectd.',
+  $graphitepostfix           = undef,
+  $interval                  = undef,
+  $escapecharacter           = '_',
+  Boolean $alwaysappendds    = false,
+  $protocol                  = 'tcp',
+  Boolean $separateinstances = false,
+  Boolean $logsenderrors     = true,
 ) {
 
   include ::collectd
   include ::collectd::plugin::write_graphite
-
-  validate_bool($storerates)
-  validate_bool($alwaysappendds)
-  validate_bool($separateinstances)
-  validate_bool($logsenderrors)
 
   concat::fragment { "collectd_plugin_write_graphite_conf_${title}_${protocol}_${graphiteport}":
     order   => '50', # somewhere between header and footer
