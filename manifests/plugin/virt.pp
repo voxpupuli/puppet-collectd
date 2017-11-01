109b026ba2d38c9c91302ab0b0924b9fddf483cb
Tim Meusel <tim@bastelfreak.de>
2017-11-01 15:25:25 +0100

    replace validate_* with datatypes in virt

--- manifests/plugin/virt.pp
+++ manifests/plugin/virt.pp
@@ -1,31 +1,21 @@
 # https://collectd.org/wiki/index.php/Plugin:virt
 class collectd::plugin::virt (
-  $connection,
-  $ensure           = 'present',
-  $manage_package   = undef,
-  $refresh_interval = undef,
-  $domain           = undef,
-  $block_device     = undef,
-  $interface_device = undef,
-  $ignore_selected  = undef,
-  $hostname_format  = undef,
-  $interface_format = undef,
-  $interval         = undef,
+  String $connection,
+  $ensure                                      = 'present',
+  $manage_package                              = undef,
+  Optional[Pattern[/^\d+$/]] $refresh_interval = undef,
+  Optional[String] $domain                     = undef,
+  Optional[String] $block_device               = undef,
+  Optional[String]$interface_device            = undef,
+  Optional[Boolean] $ignore_selected           = undef,
+  Optional[String] $hostname_format            = undef,
+  Optional[String] $interface_format           = undef,
+  $interval                                    = undef,
 ) {
 
   include ::collectd
 
   $_manage_package = pick($manage_package, $::collectd::manage_package)
-
-  validate_string($connection)
-
-  if $refresh_interval != undef { validate_re($refresh_interval, '^\d+$') }
-  if $domain != undef           { validate_string($domain) }
-  if $block_device != undef     { validate_string($block_device) }
-  if $interface_device != undef { validate_string($interface_device) }
-  if $ignore_selected != undef  { validate_bool($ignore_selected) }
-  if $hostname_format != undef  { validate_string($hostname_format) }
-  if $interface_format != undef { validate_string($interface_format) }
 
   if $::osfamily == 'RedHat' {
     if $_manage_package {
