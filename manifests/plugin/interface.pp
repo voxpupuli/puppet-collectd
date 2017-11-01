8ece9b2555f688d7a30452b5fcf27343af473aa4
Tim Meusel <tim@bastelfreak.de>
2017-11-01 14:39:48 +0100

    replace validate_* with datatypes in interface

--- manifests/plugin/interface.pp
+++ manifests/plugin/interface.pp
@@ -1,19 +1,13 @@
 # https://collectd.org/wiki/index.php/Plugin:Interface
 class collectd::plugin::interface (
-  $ensure         = 'present',
-  $interfaces     = [],
-  $ignoreselected = false,
-  $reportinactive = true,
-  $interval       = undef,
+  $ensure                 = 'present',
+  Array $interfaces       = [],
+  Boolean $ignoreselected = false,
+  Boolean $reportinactive = true,
+  $interval               = undef,
 ) {
 
   include ::collectd
-
-  validate_array($interfaces)
-  validate_bool(
-    $ignoreselected,
-    $reportinactive,
-  )
 
   collectd::plugin { 'interface':
     ensure   => $ensure,
