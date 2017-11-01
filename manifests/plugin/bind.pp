f6e0557bd3d76c83ef27855bdb19517906cdc79c
Tim Meusel <tim@bastelfreak.de>
2017-11-01 13:14:05 +0100

    replace validate_* with datatypes in bind

--- manifests/plugin/bind.pp
+++ manifests/plugin/bind.pp
@@ -1,33 +1,22 @@
 # https://collectd.org/wiki/index.php/Plugin:BIND
 class collectd::plugin::bind (
   $url,
-  $ensure         = 'present',
-  $manage_package = undef,
-  $memorystats    = true,
-  $opcodes        = true,
-  $parsetime      = false,
-  $qtypes         = true,
-  $resolverstats  = false,
-  $serverstats    = true,
-  $zonemaintstats = true,
-  $views          = [],
-  $interval       = undef,
+  $ensure                 = 'present',
+  $manage_package         = undef,
+  Boolean $memorystats    = true,
+  Boolean $opcodes        = true,
+  Boolean $parsetime      = false,
+  Boolean $qtypes         = true,
+  Boolean $resolverstats  = false,
+  Boolean $serverstats    = true,
+  Boolean $zonemaintstats = true,
+  Array $views            = [],
+  $interval               = undef,
 ) {
 
   include ::collectd
 
   $_manage_package = pick($manage_package, $::collectd::manage_package)
-
-  validate_bool(
-    $memorystats,
-    $opcodes,
-    $parsetime,
-    $qtypes,
-    $resolverstats,
-    $serverstats,
-    $zonemaintstats,
-  )
-  validate_array($views)
 
   if $::osfamily == 'RedHat' {
     if $_manage_package {
