b9cf24e88ce9b9214a4a2a0653d24bef3e2c8440
Tim Meusel <tim@bastelfreak.de>
2017-11-01 14:33:06 +0100

    replace validate_* with datatypes in repo

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -3,10 +3,6 @@
 class collectd::repo {
 
   if $collectd::manage_repo {
-    if $::collectd::ci_package_repo != undef {
-      validate_re($::collectd::ci_package_repo, [ '^5.4', '^5.5', '^5.6', '5.7', '^master' ], "ci_package_repo has to match '5.4', '5.5', '5.6', '5.7' or 'master' (RC for next release), got: ${::collectd::ci_package_repo}")
-    }
-
     $osfamily_downcase = downcase($::osfamily)
 
     if defined("::collectd::repo::${osfamily_downcase}") {
