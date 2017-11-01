f08bab5b809663b73835e7bc825ddfc94258ce41
Tim Meusel <tim@bastelfreak.de>
2017-11-01 13:07:31 +0100

    replace validate_* with datatypes in amqp

--- manifests/plugin/amqp.pp
+++ manifests/plugin/amqp.pp
@@ -1,29 +1,27 @@
 # https://collectd.org/wiki/index.php/Plugin:AMQP
 class collectd::plugin::amqp (
-  $ensure                     = 'present',
-  $manage_package             = undef,
-  $amqphost                   = 'localhost',
-  $amqpport                   = 5672,
-  $amqpvhost                  = 'graphite',
-  $amqpuser                   = 'graphite',
-  $amqppass                   = 'graphite',
-  $amqpformat                 = 'Graphite',
-  $amqpstorerates             = false,
-  $amqpexchange               = 'metrics',
-  $amqppersistent             = true,
-  $amqproutingkey             = 'collectd',
-  $graphiteprefix             = 'collectd.',
-  $escapecharacter            = '_',
-  $interval                   = undef,
-  $graphiteseparateinstances  = false,
-  $graphitealwaysappendds     = false,
+  $ensure                            = 'present',
+  $manage_package                    = undef,
+  $amqphost                          = 'localhost',
+  $amqpport                          = 5672,
+  $amqpvhost                         = 'graphite',
+  $amqpuser                          = 'graphite',
+  $amqppass                          = 'graphite',
+  $amqpformat                        = 'Graphite',
+  $amqpstorerates                    = false,
+  $amqpexchange                      = 'metrics',
+  Boolean $amqppersistent            = true,
+  $amqproutingkey                    = 'collectd',
+  $graphiteprefix                    = 'collectd.',
+  $escapecharacter                   = '_',
+  $interval                          = undef,
+  Boolean $graphiteseparateinstances = false,
+  Boolean $graphitealwaysappendds    = false,
 ) {
 
   include ::collectd
 
   $_manage_package = pick($manage_package, $::collectd::manage_package)
-
-  validate_bool($amqppersistent, $graphiteseparateinstances, $graphitealwaysappendds)
 
   if $::osfamily == 'RedHat' {
     if $_manage_package {
