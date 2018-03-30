d616699ae6a572dab0b5a4eada939a4903a1fef6
juniorsysadmin <juniorsysadmin@users.noreply.github.com>
2018-03-30 16:31:13 +1100

    Remove parameters from collectd::service class
    Use variables directly from the init.pp

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,17 +1,13 @@
-class collectd::service (
-  $service_name   = $collectd::service_name,
-  $service_ensure = $collectd::service_ensure,
-  $service_enable = $collectd::service_enable,
-  $manage_service = $collectd::manage_service,
-) {
+#
+class collectd::service {
 
   assert_private()
 
-  if $manage_service {
+  if $collectd::manage_service {
     service { 'collectd':
-      ensure => $service_ensure,
-      name   => $service_name,
-      enable => $service_enable,
+      ensure => $collectd::service_ensure,
+      name   => $collectd::service_name,
+      enable => $collectd::service_enable,
     }
   }
 
