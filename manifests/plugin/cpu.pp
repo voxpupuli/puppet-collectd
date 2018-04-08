500b7eb17c4b6797b97a4023e2bb6323be6355d1
juniorsysadmin <juniorsysadmin@users.noreply.github.com>
2018-04-08 14:07:30 +1000

    collectd::plugin::cpu - Use data types

--- manifests/plugin/cpu.pp
+++ manifests/plugin/cpu.pp
@@ -1,11 +1,11 @@
 # https://collectd.org/wiki/index.php/Plugin:CPU
 class collectd::plugin::cpu (
-  $ensure           = 'present',
-  Boolean $reportbystate    = true,
-  Boolean $reportbycpu      = true,
-  Boolean $valuespercentage = false,
-  Boolean $reportnumcpu     = false,
-  $interval         = undef,
+  Enum['present', 'absent'] $ensure = 'present',
+  Boolean $reportbystate            = true,
+  Boolean $reportbycpu              = true,
+  Boolean $valuespercentage         = false,
+  Boolean $reportnumcpu             = false,
+  Optional[Integer[1]] $interval    = undef,
 ) {
 
   include ::collectd
