73500e260f9fab9a38fe1e781d2d37255be1deec
Romain Tartière <romain@blogreen.org>
2018-05-21 19:32:14 +0200

    Align default values after last commit

--- manifests/plugin/bind.pp
+++ manifests/plugin/bind.pp
@@ -1,17 +1,17 @@
 # https://collectd.org/wiki/index.php/Plugin:BIND
 class collectd::plugin::bind (
   Stdlib::Httpurl $url,
-  Enum['present', 'absent'] $ensure = 'present',
-  Boolean $manage_package           = $collectd::manage_package,
-  Boolean $memorystats              = true,
-  Boolean $opcodes                  = true,
-  Boolean $parsetime                = false,
-  Boolean $qtypes                   = true,
-  Boolean $resolverstats            = false,
-  Boolean $serverstats              = true,
-  Boolean $zonemaintstats           = true,
+  Enum['present', 'absent'] $ensure  = 'present',
+  Boolean $manage_package            = $collectd::manage_package,
+  Boolean $memorystats               = true,
+  Boolean $opcodes                   = true,
+  Boolean $parsetime                 = false,
+  Boolean $qtypes                    = true,
+  Boolean $resolverstats             = false,
+  Boolean $serverstats               = true,
+  Boolean $zonemaintstats            = true,
   Array[Collectd::Bind::View] $views = [],
-  Optional[Integer[1]] $interval    = undef,
+  Optional[Integer[1]] $interval     = undef,
 ) {
 
   include ::collectd
