7f558e4a79ba870ebd538007b5a239697d8337b9
Mehdi Abaakouk <sileht@sileht.net>
2018-06-28 11:41:13 +0200

    Add ipc plugin
    Collectd plugin ipc have been added in Collectd 5.5.
    
    This change adds its support to puppet-collectd

---
+++ manifests/plugin/ipc.pp
@@ -0,0 +1,23 @@
+#== Class: collectd::plugin::ipc
+#
+# Class to manage ipc write plugin for collectd
+#
+# Documentation:
+#   https://collectd.org/documentation/manpages/collectd.conf.5.shtml#plugin_ipc
+#
+# === Parameters
+#
+# [*ensure*]
+#  Ensure param for collectd::plugin type.
+#  Defaults to 'ensure'
+#
+class collectd::plugin::ipc (
+  Enum['present', 'absent'] $ensure   = 'present',
+) {
+
+  include collectd
+
+  collectd::plugin { 'ipc':
+    ensure   => $ensure,
+  }
+}
