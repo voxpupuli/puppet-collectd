dd7fb6d4fc8bb72301fd4ed80d894b787931f3e1
Romain Tartière <romain@blogreen.org>
2018-07-15 15:46:18 +0200

    Add missing data types for the threshold plugin
    While here, align all parameters and default values.

--- manifests/plugin/threshold.pp
+++ manifests/plugin/threshold.pp
@@ -1,10 +1,10 @@
 # http://collectd.org/documentation/manpages/collectd.conf.5.shtml#plugin_threshold
 class collectd::plugin::threshold (
-  $ensure   = 'present',
-  $interval = undef,
-  Array[Collectd::Threshold::Type]   $types   = [],
-  Array[Collectd::Threshold::Plugin] $plugins = [],
-  Array[Collectd::Threshold::Host]   $hosts   = [],
+  Enum['present', 'absent']          $ensure   = 'present',
+  Optional[Integer]                  $interval = undef,
+  Array[Collectd::Threshold::Type]   $types    = [],
+  Array[Collectd::Threshold::Plugin] $plugins  = [],
+  Array[Collectd::Threshold::Host]   $hosts    = [],
 ) {
 
   include ::collectd
