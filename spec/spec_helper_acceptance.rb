2070c3f06472e8e290ef5882f81e8c4bae92a31a
Tim Meusel <tim@bastelfreak.de>
2018-08-11 22:34:55 +0200

    clean spec_helper_acceptance

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,14 +1,11 @@
-require 'beaker-rspec/spec_helper'
-require 'beaker-rspec/helpers/serverspec'
+require 'beaker-rspec'
+require 'beaker-puppet'
 require 'beaker/puppet_install_helper'
 require 'beaker/module_install_helper'
 
 run_puppet_install_helper unless ENV['BEAKER_provision'] == 'no'
 
 RSpec.configure do |c|
-  # Project root
-  proj_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
-
   install_module_on(hosts)
   install_module_dependencies_on(hosts)
 
@@ -17,8 +14,6 @@ RSpec.configure do |c|
 
   # Configure all nodes in nodeset
   c.before :suite do
-    # Install module and dependencies
-    puppet_module_install(source: proj_root, module_name: 'collectd')
     hosts.each do |host|
       # python is pre-requisite to the python_path fact.
       on host, puppet('resource', 'package', 'python', 'ensure=installed')
