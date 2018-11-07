62cee1584307ee8b895e3e7d02d370254dd95025
Tim Schaller <tschaller@syntheticgenomics.com>
2018-11-07 15:34:52 -0800

    Fix for issue #653

--- manifests/repo/redhat.pp
+++ manifests/repo/redhat.pp
@@ -2,6 +2,13 @@ class collectd::repo::redhat {
 
   if $::collectd::ci_package_repo {
 
+    # Fix for issue #653
+    if $::collectd::ci_package_repo != 'master' {
+      $path_prefix = 'collectd-'
+    } else {
+      $path_prefix = ''
+    }
+      
     yumrepo { 'collectd-ci':
       ensure  => present,
       enabled => '1',
