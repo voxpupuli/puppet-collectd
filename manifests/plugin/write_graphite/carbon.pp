aea1d1030b415748ed8140de9e223354701df6ff
markasammut <mark.sammut@gamesysnetwork.com>
2019-10-03 11:36:00 +0200

    Add ReconnectInterval parameter for write_graphite plugin

--- manifests/plugin/write_graphite/carbon.pp
+++ manifests/plugin/write_graphite/carbon.pp
@@ -12,6 +12,7 @@ define collectd::plugin::write_graphite::carbon (
   $protocol                  = 'tcp',
   Boolean $separateinstances = false,
   Boolean $logsenderrors     = true,
+  $reconnectinterval         = 0,
 ) {
 
   include collectd
