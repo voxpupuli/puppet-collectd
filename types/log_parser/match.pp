2228727ce5b9103cfd5bf517e542e317ed03282e
michalrebisz <michalx.rebisz@intel.com>
2020-02-13 14:22:27 +0100

    added default config

--- types/log_parser/match.pp
+++ types/log_parser/match.pp
@@ -1,7 +1,7 @@
 #https://wiki.opnfv.org/display/fastpath/Logparser+plugin+HLD
 type Collectd::LOG_PARSER::Match = Struct[{
   'regex' => String,
-  Optional['submatchidx'] => String,
+  Optional['submatchidx'] => Integer,
   Optional['excluderegex'] => String,
   Optional['ismandatory'] => Boolean,
   Optional['severity'] => String,
