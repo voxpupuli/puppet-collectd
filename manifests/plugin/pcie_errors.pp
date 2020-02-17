f36c06f665a46e0990cdea1db695161255b1083c
Piotr Rabiega <piotrx.rabiega@intel.com>
2020-02-17 09:16:45 +0100

    Add puppet string documentation

--- manifests/plugin/pcie_errors.pp
+++ manifests/plugin/pcie_errors.pp
@@ -1,9 +1,17 @@
+# Class to manage pcie_errors plugin for collectd
+#
+# @param ensure Ensure param for collectd::plugin type.
+# @param source Use sysfs or proc to read data from /sysfs or /proc.
+# @param access_dir Directory used to access device config space.
+# @param report_masked If true plugin will notify about errors that are set to masked in Error Mask register.
+# @param persistent_notifications If false plugin will dispatch notification only on set/clear of error.
+#
 class collectd::plugin::pcie_errors (
   Enum['present', 'absent'] $ensure                   = 'present',
   Optional[String]          $source                   = 'sysfs',
   Optional[String]          $access_dir               = undef,
-  Optional[Boolean]         $report_masked            = false,
-  Optional[Boolean]         $persistent_notifications = false,
+  Boolean                   $report_masked            = false,
+  Boolean                   $persistent_notifications = false,
 ) {
 
   include collectd
