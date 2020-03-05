3f479c1695c86a144674fb1d153feff736c9dea4
MichalRebisz <michalx.rebisz@intel.com>
2020-03-06 00:23:32 +0100

    Added test for checking file is correct

--- spec/classes/collectd_plugin_log_parser_spec.rb
+++ spec/classes/collectd_plugin_log_parser_spec.rb
@@ -19,41 +19,13 @@ describe 'collectd::plugin::log_parser', type: :class do
         end
       end
 
-      context ':ensure => log parser created with logfile' do
-        let :params do
-          {
-            logfile: [
-              'file01.log' => {
-                'message' => [
-                  'msg_1' => {
-                    'defaultplugininstance' => 'plugin_instance',
-                    'defaulttype' => 'type',
-                    'defaulttypeinstance' => 'type_instance',
-                    'defaultseverity' => 'ok',
-                    'match' => [
-                      'sample_error' => {
-                        'regex' => 'regex',
-                        'submatchidx' => 0,
-                        'excluderegex' => 'exclude',
-                        'ismandatory' => false,
-                        'severity' => 'severity',
-                        'plugininstance' => 'plugin_instance',
-                        'type' => 'type',
-                        'typeinstance' => 'type_instance'
-                      }
-                    ]
-                  }
-                ],
-                'firstfullread' => false
-              }
-            ]
-          }
-        end
-
+      context ':ensure => log parser created with default values' do
+        default_fixture = File.read(fixtures('plugins/logparser.conf.default'))
         it "Will create #{options[:plugin_conf_dir]}/06-log_parser.conf" do
           is_expected.to contain_file('log_parser.load').with(
             ensure: 'present',
-            path: "#{options[:plugin_conf_dir]}/06-log_parser.conf"
+            path: "#{options[:plugin_conf_dir]}/06-log_parser.conf",
+            content: default_fixture
           )
         end
       end
