88a26d47e36a5c7712ef6f6e136cb4f35953abfb
Matthias Runge <mrunge@redhat.com>
2020-08-05 11:25:40 +0200

    Change SendQueueLimit to positive integer
    including 0. The parameter should not become negative.

--- manifests/plugin/amqp1.pp
+++ manifests/plugin/amqp1.pp
@@ -104,18 +104,18 @@
 #      preserved, i.e. passed through.
 #
 class collectd::plugin::amqp1 (
-  Enum['present', 'absent'] $ensure   = 'present',
-  Boolean $manage_package             = $collectd::manage_package,
-  String $transport                   = 'metrics',
-  Stdlib::Host $host                  = 'localhost',
-  Stdlib::Port $port                  = 5672,
-  String $user                        = 'guest',
-  String $password                    = 'guest',
-  String $address                     = 'collectd',
-  Hash $instances                     = {},
-  Optional[Integer] $retry_delay      = undef,
-  Optional[Integer] $send_queue_limit = undef,
-  Optional[Integer] $interval         = undef,
+  Enum['present', 'absent'] $ensure      = 'present',
+  Boolean $manage_package                = $collectd::manage_package,
+  String $transport                      = 'metrics',
+  Stdlib::Host $host                     = 'localhost',
+  Stdlib::Port $port                     = 5672,
+  String $user                           = 'guest',
+  String $password                       = 'guest',
+  String $address                        = 'collectd',
+  Hash $instances                        = {},
+  Optional[Integer] $retry_delay         = undef,
+  Optional[Integer[0]] $send_queue_limit = undef,
+  Optional[Integer] $interval            = undef,
 ) {
   include collectd
 
