20494e44a90073273a18fce71f4a602d5b5d0690
Leif Madsen <lmadsen@redhat.com>
2020-09-11 10:18:58 -0400

    Add PluginInstanceFormat parameter to virt pluginAdd the PluginInstanceFormat parameter to the virt plugin to allow forspecifying the plugin instance format, resulting in a new label beingavailable to the virt metrics. By adding this you can provide the UUIDof the virtual machine, along with HostnameFormat to have informationabout VMs that exist per physical host.Adds spec test to validate that PluginInstanceFormat is available incollectd version 5.5.0 or later.

--- spec/classes/collectd_plugin_virt_spec.rb
+++ spec/classes/collectd_plugin_virt_spec.rb
@@ -11,6 +11,57 @@ describe 'collectd::plugin::virt', type: :class do
         'include collectd'
       end
 
+      context 'plugin_instance_format in virt.conf' do
+        let :params do
+          {
+            connection: 'qemu:///system',
+            plugin_instance_format: 'name'
+          }
+        end
+
+        context 'with collectd_version < 5.0' do
+          let :facts do
+            facts.merge(collectd_version: '4.10.1')
+          end
+
+          it 'is ignored' do
+            is_expected.to contain_file('libvirt.load').
+              without_content(%r{.*PluginInstanceFormat name.*})
+          end
+        end
+
+        context 'with collectd_version >= 5.0' do
+          let :facts do
+            facts.merge(collectd_version: '5.0.0')
+          end
+
+          it 'is included' do
+            is_expected.to contain_file('libvirt.load').
+              without_content(%r{.*PluginInstanceFormat name.*})
+          end
+        end
+
+        context 'with collectd_version >= 5.5.0' do
+          let :facts do
+            facts.merge(collectd_version: '5.5.0')
+          end
+
+          it 'is included' do
+            is_expected.to contain_file('virt.load').
+              with_content(%r{.*PluginInstanceFormat name.*})
+          end
+        end
+
+        case facts[:os]['family']
+        when 'RedHat'
+          context 'on osfamily => RedHat' do
+            it 'Will delete packaging config file' do
+              is_expected.to contain_file('package_virt.load').with_ensure('absent')
+            end
+          end
+        end
+      end
+
       context 'interface_format in virt.conf' do
         let :params do
           {
