2f2eee243002afac5b6f9a3947814f0346cf44b1
Tim Meusel <tim@bastelfreak.de>
2021-04-26 22:46:07 +0200

    Update manifests/plugin/virt.pp

--- manifests/plugin/virt.pp
+++ manifests/plugin/virt.pp
@@ -8,7 +8,7 @@ class collectd::plugin::virt (
   Optional[String] $block_device                                             = undef,
   Optional[String] $interface_device                                         = undef,
   Optional[Boolean] $ignore_selected                                         = undef,
-  Optional[String[1]] $plugin_instance_format                                   = undef,
+  Optional[String[1]] $plugin_instance_format                                = undef,
   Optional[String] $hostname_format                                          = undef,
   Optional[String] $interface_format                                         = undef,
   Optional[String] $extra_stats                                              = undef,
