80c07ad729af0b253ba428bf2a2260172a210137
Romain Tartière <romain@blogreen.org>
2021-09-10 13:56:30 -1000

    Remove unreliable acceptance tests
    These tests are very fragile: they depend on the version of collectd,
    the version of python bundled with collectd, the version of python
    available on the system.  Sometimes these version match, more often they
    don't.  These tests also depend on external projects which are not
    actively maintained anymore and we face race conditions hard to debug
    that make the CI fail.
    
    Since the aspect we are already interested in are already tested with
    puppet-rspec, drop these test which depends on too much external things.

--- spec/acceptance/class_spec.rb
+++ spec/acceptance/class_spec.rb
@@ -31,30 +31,12 @@ describe 'collectd class' do
 
       class { '::collectd::plugin::memory': }
 
-      # rabbitmq plugin not ported to Python3
-      unless $facts['os']['family'] == 'RedHat' and $facts['os']['release']['major'] == '8' {
-        class { '::collectd::plugin::rabbitmq': }
-      }
       class { 'collectd::plugin::csv':}
       EOS
 
       # Run it twice and test for idempotency
       apply_manifest(pp, catch_failures: true)
       apply_manifest(pp, catch_changes: true)
-    end
-
-    if fact('osfamily') == 'Debian'
-      describe file('/etc/collectd/conf.d/10-rabbitmq.conf') do
-        it { is_expected.to be_file }
-        it { is_expected.to contain 'TypesDB "/usr/local/share/collectd-rabbitmq/types.db.custom"' }
-      end
-    end
-
-    if fact('osfamily') == 'RedHat' && fact('os.release.major') == '7'
-      describe file('/etc/collectd.d/10-rabbitmq.conf') do
-        it { is_expected.to be_file }
-        it { is_expected.to contain 'TypesDB "/usr/share/collectd-rabbitmq/types.db.custom"' }
-      end
     end
 
     describe service('collectd') do
