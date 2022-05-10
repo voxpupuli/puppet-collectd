4964e2c666980a0b04e0c45efd5eb905c65b4451
wroose <wim.roose@ugent.be>
2022-05-10 14:38:43 +0200

    add support for RegisterCmd modbus option

--- types/modbus/data.pp
+++ types/modbus/data.pp
@@ -1,2 +1,8 @@
 #
-type Collectd::Modbus::Data = Struct[{Optional['instance'] => String, NotUndef['type'] => String[1], NotUndef['register_base'] => Numeric, NotUndef['register_type'] => String[1]}]
+type Collectd::Modbus::Data = Struct[{
+  Optional['instance']      => String,
+  NotUndef['type']          => String[1],
+  NotUndef['register_base'] => Integer[0],
+  NotUndef['register_type'] => Enum['Int16', 'Int32', 'Uint16', 'Uint32', 'Float'],
+  Optional['register_cmd']  => Enum['ReadHolding', 'ReadInput'],
+}]
