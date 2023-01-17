cb1f46f353cc279d586abbfe978b2008ed9aac44
Sergei Gerasenko <sgerasenko@conversantmedia.com>
2023-01-17 15:51:57 +0000

    Add class docs in parseable format

--- manifests/plugin/write_prometheus.pp
+++ manifests/plugin/write_prometheus.pp
@@ -1,3 +1,11 @@
+# Class: collectd::plugin::write_prometheus
+#
+# @see https://collectd.org/wiki/index.php/Plugin:Write_Prometheus
+
+# Configures write_prometheus plugin.
+#
+# @param port Stdlib::Port Defines the port on which to accept scrape requests from Prometheus. Default: 9103
+# @param ip Optional[Stdlib::IP::Address] Defines the IP address to bind to. In not specified, the listener will bind to all IPs present.
 class collectd::plugin::write_prometheus (
   Stdlib::Port $port = 9103,
   Optional[Stdlib::IP::Address] $ip = undef,
