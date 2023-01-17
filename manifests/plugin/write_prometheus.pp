fdd8b3ed29a2c2aa4169f9ce17d58b55a7b57fe9
Tim Meusel <tim@bastelfreak.de>
2023-01-17 21:18:57 +0100

    Update manifests/plugin/write_prometheus.pp

--- manifests/plugin/write_prometheus.pp
+++ manifests/plugin/write_prometheus.pp
@@ -4,8 +4,8 @@
 
 # Configures write_prometheus plugin.
 #
-# @param port Stdlib::Port Defines the port on which to accept scrape requests from Prometheus. Default: 9103
-# @param ip Optional[Stdlib::IP::Address] Defines the IP address to bind to. In not specified, the listener will bind to all IPs present.
+# @param port Defines the port on which to accept scrape requests from Prometheus.
+# @param ip Defines the IP address to bind to. In not specified, the listener will bind to all IPs present.
 class collectd::plugin::write_prometheus (
   Stdlib::Port $port = 9103,
   Optional[Stdlib::IP::Address] $ip = undef,
